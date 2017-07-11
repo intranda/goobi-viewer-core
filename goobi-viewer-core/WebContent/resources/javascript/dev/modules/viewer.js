e1574d73aa6b18345ba35e60515c1b3684e8a6a6
Marc Lettau-Poelchen <marc.lettau-poelchen@intranda.com>
2017-07-11 15:37:03 +0200

    added popovers for view calendar

--- goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewer.js
+++ goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewer.js
@@ -370,6 +370,81 @@
 ;var viewerJS = ( function( viewer ) {
     'use strict';
     
+    var _debug = true;
+    var _this = null;
+    var _currApiCall = '';
+    var _json = {};
+    var _popoverConfig = {};
+    var _popoverContent = null;
+    var _defaults = {
+        appUrl: '',
+        popoverTriggerSelector: '[data-popover-trigger="calendar-po-trigger"]',
+        popoverTitle: 'Bitte übergeben Sie den Titel des Werks',
+    };
+    
+    viewer.calendarPopover = {
+        init: function( config ) {
+            if ( _debug ) {
+                console.log( '##############################' );
+                console.log( 'viewer.calendarPopover.init' );
+                console.log( '##############################' );
+                console.log( 'viewer.calendarPopover.init: config - ', config );
+            }
+            
+            $.extend( true, _defaults, config );
+            
+            $( _defaults.popoverTriggerSelector ).on( 'click', function() {
+                _this = $( this );
+                _currApiCall = encodeURI( _this.attr( 'data-api' ) );
+                
+                viewerJS.helper.getRemoteData( _currApiCall ).done( function( _json ) {
+                    _popoverContent = _getPopoverContent( _json, _defaults );
+                    _popoverConfig = {
+                        placement: 'auto bottom',
+                        title: _defaults.popoverTitle,
+                        content: _popoverContent,
+                        html: true
+                    };
+                    
+                    _this.off().popover( _popoverConfig );
+                } );
+            } );
+        }
+    };
+    
+    function _getPopoverContent( data, config ) {
+        if ( _debug ) {
+            console.log( '---------- _getPopoverContent() ----------' );
+            console.log( '_getPopoverContent: data = ', data );
+            console.log( '_getPopoverContent: config = ', config );
+        }
+        
+        var workList = '';
+        var workListLink = '';
+        
+        workList += '<ul class="list">';
+        
+        $.each( data, function( works, values ) {
+            workListLink = config.appUrl + 'image/' + values.PI_TOPSTRUCT + '/' + values.THUMBPAGENO + '/' + values.LOGID + '/';
+            
+            workList += '<li>';
+            workList += '<a href="' + workListLink + '">';
+            workList += values.LABEL;
+            workList += '</a>';
+            workList += '</li>';
+        } );
+        
+        workList += '</ul>';
+        
+        return workList;
+    }
+    
+    return viewer;
+    
+} )( viewerJS || {}, jQuery );
+;var viewerJS = ( function( viewer ) {
+    'use strict';
+    
     var _debug = false;
     var _parsedFontSize = 0;
     var _currFontSize = '';
