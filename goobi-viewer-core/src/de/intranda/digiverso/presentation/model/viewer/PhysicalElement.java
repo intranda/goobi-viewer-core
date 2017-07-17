5f0c37e353c1c5d286d2641a8fa8aed8656ba60e
Florian Alpers <florian.alpers@intranda.com>
2017-07-17 13:49:30 +0200

    added fop.jar (apache licence) display single page pdf download icon regardless of external pdf generation pass mets path to single page pdf download

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/model/viewer/PhysicalElement.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/model/viewer/PhysicalElement.java
@@ -926,6 +926,11 @@ public class PhysicalElement implements Comparable<PhysicalElement>, Serializabl
     }
 
     public String getImageToPdfUrl() {
+        StringBuilder sb = new StringBuilder(DataManager.getInstance().getConfiguration().getContentServerWrapperUrl());
+        sb.append("?action=pdf")
+        .append("&images=").append(pi).append("/").append(fileName)
+        .append("&metsFile=").append(pi).append(".xml")
+        .append("&targetFileName=").append(pi).append("_").append(order).append(".pdf");
         return DataManager.getInstance().getConfiguration().getContentServerWrapperUrl() + "?action=pdf&images=" + pi + "/" + fileName
                 + "&targetFileName=" + pi + "_" + order + ".pdf";
     }
