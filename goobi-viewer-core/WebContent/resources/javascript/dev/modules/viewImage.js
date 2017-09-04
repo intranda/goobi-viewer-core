40b751bd5c665c49097219905ba5cafdadc63d96
Marc Lettau-Poelchen <marc.lettau-poelchen@intranda.com>
2017-09-04 16:24:44 +0200

    added new cms template for static grid

--- goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewImage.js
+++ goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewImage.js
@@ -1,20 +1,17 @@
-/*
- * ! This file is part of the Goobi viewer - a content presentation and management
- * application for digitized objects.
- * 
- * Visit these websites for more information. - http://www.intranda.com -
- * http://digiverso.com
- * 
- * This program is free software; you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this
- * program. If not, see <http://www.gnu.org/licenses/>.
+/*!
+ * This file is part of the Goobi viewer - a content presentation and management application for digitized objects.
+ *
+ * Visit these websites for more information.
+ * - http://www.intranda.com
+ * - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */var viewImage = ( function() {
     'use strict';
     
@@ -334,37 +331,37 @@
         },
         getScaleToOriginalSize: function(imageNo) {
         	return 1.0;
-// if(!imageNo) {
-// imageNo = 0;
-// }
-// var displaySize = osViewer.viewer.viewport._contentSize.x;
-// return osViewer.getImageInfo()[imageNo].tileSource.width / displaySize;
+//        	if(!imageNo) {
+//        		imageNo = 0;
+//        	}
+//            var displaySize = osViewer.viewer.viewport._contentSize.x;
+//            return osViewer.getImageInfo()[imageNo].tileSource.width / displaySize;
         },
         scaleToOriginalSize: function( value, imageNo ) {
         	return value;
-// if ( _debug ) {
-// console.log( 'Overlays _scaleToOriginalSize: value - ' + value );
-// }
+//            if ( _debug ) {
+//                console.log( 'Overlays _scaleToOriginalSize: value - ' + value );
+//            }
 //            
-// if(!imageNo) {
-// imageNo = 0;
-// }
+//            if(!imageNo) {
+//        		imageNo = 0;
+//        	}
 //            
-// var displaySize = osViewer.viewer.viewport._contentSize.x;
-// return value / displaySize * osViewer.getImageInfo()[imageNo].tileSource.width;
+//            var displaySize = osViewer.viewer.viewport._contentSize.x;
+//            return value / displaySize * osViewer.getImageInfo()[imageNo].tileSource.width;
         },
         scaleToImageSize: function( value, imageNo ) {
         	return value;
-// if ( _debug ) {
-// console.log( 'Overlays _scaleToImageSize: value - ' + value );
-// }
+//            if ( _debug ) {
+//                console.log( 'Overlays _scaleToImageSize: value - ' + value );
+//            }
 //            
-// if(!imageNo) {
-// imageNo = 0;
-// }
+//            if(!imageNo) {
+//        		imageNo = 0;
+//        	}
 //            
-// var displaySize = osViewer.viewer.viewport._contentSize.x;
-// return value * displaySize / osViewer.getImageInfo()[imageNo].tileSource.width;
+//            var displaySize = osViewer.viewer.viewport._contentSize.x;
+//            return value * displaySize / osViewer.getImageInfo()[imageNo].tileSource.width;
         },
         close: function() {
             if ( _debug ) {
