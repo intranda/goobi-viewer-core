24a3edf5bfb3db5ae1a5cbec583e703505c23c53
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2017-10-27 15:37:26 +0200

    remove <br> tags from fulltext fragments; fixes #15862

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/model/search/SearchHit.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/model/search/SearchHit.java
@@ -534,7 +534,6 @@ public class SearchHit implements Comparable<SearchHit> {
     public boolean isHasHitCount() {
         for (HitType key : hitTypeCounts.keySet()) {
             if (hitTypeCounts.get(key) > 0) {
-                logger.trace("isHasHitCount: true");
                 return true;
             }
         }
