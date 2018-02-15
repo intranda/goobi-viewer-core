8e412486379bdf69393215cbaa5c1957ee075f87
Florian Alpers <florian.alpers@intranda.com>
2018-02-15 09:38:25 +0100

    partially added pdf delivery urls

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/controller/imaging/ImageDeliveryManager.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/controller/imaging/ImageDeliveryManager.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 import javax.inject.Inject;
 
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,80 +41,90 @@ import de.unigoettingen.sub.commons.contentlib.servlet.model.iiif.ImageInformati
  *
  */
 public class ImageDeliveryManager {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ImageDeliveryManager.class);
 
     @Inject
     private ActiveDocumentBean activeDocumentBean;
 
     private Optional<PhysicalElement> getCurrentPageIfExists() {
-      return Optional.ofNullable(activeDocumentBean)
-              .map(adb -> adb.getViewManager())
-              .map(vm -> {
-                try {
-                    return vm.getCurrentPage();
-                } catch (IndexUnreachableException | DAOException e) {
-                    logger.error(e.toString());
-                    return null;
-                }
-            });
+        return Optional.ofNullable(activeDocumentBean)
+                .map(adb -> adb.getViewManager())
+                .map(vm -> {
+                    try {
+                        return vm.getCurrentPage();
+                    } catch (IndexUnreachableException | DAOException e) {
+                        logger.error(e.toString());
+                        return null;
+                    }
+                });
     }
-    
+
     private Optional<ActiveDocumentBean> getActiveDocumentBeanIfExists() {
         return Optional.ofNullable(activeDocumentBean);
     }
-    
+
     private Optional<String> getFooterIdIfExists() {
-        return getActiveDocumentBeanIfExists()
-                .map(adb -> adb.getTopDocument())
+        return getActiveDocumentBeanIfExists().map(adb -> adb.getTopDocument())
                 .map(doc -> getFooterId(doc));
     }
 
     /**
-     * Creates the watermark url for the given pageType, adding watermarkId for the current {@link ActiveDocumentBean#getTopDocument()}
-     * and watermarkText for the current {@link PhysicalElement page}
-     * If the watermark height of the given pageType and image is 0, an empty optional is returned
+     * Creates the watermark url for the given pageType, adding watermarkId for the current {@link ActiveDocumentBean#getTopDocument()} and
+     * watermarkText for the current {@link PhysicalElement page} If the watermark height of the given pageType and image is 0, an empty optional is
+     * returned
      * 
-     * @param info          ImageInformation as basis for watermark size. Must not be null
-     * @param pageType      The pageType of the currentView. Taken into consideration for footer height, if not null
+     * @param info ImageInformation as basis for watermark size. Must not be null
+     * @param pageType The pageType of the currentView. Taken into consideration for footer height, if not null
      * @return
      * @throws IndexUnreachableException
      * @throws DAOException
      * @throws ConfigurationException
      */
-    public Optional<String> getWatermarkUrl(ImageInformation info, PageType pageType) throws IndexUnreachableException, DAOException, ConfigurationException {
-        
-        int footerHeight = DataManager.getInstance().getConfiguration().getFooterHeight(pageType, getImageType(info));
-        if(footerHeight > 0) {            
+    public Optional<String> getWatermarkUrl(ImageInformation info, PageType pageType)
+            throws IndexUnreachableException, DAOException, ConfigurationException {
+
+        int footerHeight = DataManager.getInstance()
+                .getConfiguration()
+                .getFooterHeight(pageType, getImageType(info));
+        if (footerHeight > 0) {
             String format = DataManager.getInstance()
                     .getConfiguration()
                     .getWatermarkFormat();
-            
-            Integer width = info.getSizes().stream()
-                    .sorted((size1,size2) -> Integer.compare(size2.getWidth(), size2.getWidth()))
+
+            Integer width = info.getSizes()
+                    .stream()
+                    .sorted((size1, size2) -> Integer.compare(size2.getWidth(), size2.getWidth()))
                     .map(size -> size.getWidth())
-                    .findFirst().orElse(info.getWidth());
-            
+                    .findFirst()
+                    .orElse(info.getWidth());
+
             StringBuilder urlBuilder = new StringBuilder(DataManager.getInstance()
                     .getConfiguration()
                     .getIiifUrl());
-            
+
             urlBuilder.append("footer/full/!")
-            .append(width).append(",") //width
-            .append(DataManager.getInstance().getConfiguration().getFooterHeight(pageType, getImageType(info)))
-            .append("/0/default.")
-            .append(format)
-            .append("?");
-            
-            getFooterIdIfExists().ifPresent(footerId -> urlBuilder.append("watermarkId=").append(footerId).append("&"));
-            getCurrentPageIfExists().ifPresent(page -> urlBuilder.append("&watermarkText=").append(page.getWatermarkText()));
-            
+                    .append(width)
+                    .append(",") //width
+                    .append(DataManager.getInstance()
+                            .getConfiguration()
+                            .getFooterHeight(pageType, getImageType(info)))
+                    .append("/0/default.")
+                    .append(format)
+                    .append("?");
+
+            getFooterIdIfExists().ifPresent(footerId -> urlBuilder.append("watermarkId=")
+                    .append(footerId)
+                    .append("&"));
+            getCurrentPageIfExists().ifPresent(page -> urlBuilder.append("watermarkText=")
+                    .append(page.getWatermarkText()));
+
             return Optional.of(urlBuilder.toString());
         } else {
             return Optional.empty();
         }
     }
-    
+
     private String getFooterId(StructElement topDocument) {
         String footerIdField = DataManager.getInstance()
                 .getConfiguration()
@@ -124,15 +135,67 @@ public class ImageDeliveryManager {
         }
         return footerId;
     }
-    
+
     private ImageType getImageType(ImageInformation info) {
         String id = info.getId();
         ImageFileFormat iff = ImageFileFormat.getImageFileFormatFromFileExtension(id);
-        if(iff != null) {
+        if (iff != null) {
             return new ImageType(iff);
         } else {
             return null;
         }
     }
+
+    public String getPdfUrl(PhysicalElement page) {
+        StringBuilder sb = new StringBuilder(DataManager.getInstance()
+                .getConfiguration()
+                .getIiifUrl());
+        sb.append("image")
+                .append("/")
+                .append(page.getPi())
+                .append("/")
+                .append(page.getFileName())
+                .append("/")
+                .append("full/max/0/")
+                .append(page.getPi())
+                .append("_")
+                .append(page.getOrder())
+                .append(".pdf")
+                .append("?watermarkText=")
+                .append(page.getWatermarkText());
+
+        getFooterIdIfExists().ifPresent(footerId -> sb.append("&watermarkId=")
+                .append(footerId)
+                .append("&"));
+
+        return sb.toString();
+    }
     
+    public String getPdfUrl(String pi, String divId) {
+        StringBuilder sb = new StringBuilder(DataManager.getInstance()
+                .getConfiguration()
+                .getIiifUrl());
+        sb.append("pdf/mets/")
+                .append(pi).append(".xml")
+                .append("/")
+                .append(page.getFileName())
+                .append("/");
+        
+        if(StringUtils.isNotBlank(cs))
+        
+                .append("full/max/0/")
+                .append(page.getPi())
+                .append("_")
+                .append(page.getOrder())
+                .append(".pdf")
+                .append("?watermarkText=")
+                .append(page.getWatermarkText());
+
+        getFooterIdIfExists().ifPresent(footerId -> sb.append("&watermarkId=")
+                .append(footerId)
+                .append("&"));
+
+        return sb.toString();
+    }
+
 }
