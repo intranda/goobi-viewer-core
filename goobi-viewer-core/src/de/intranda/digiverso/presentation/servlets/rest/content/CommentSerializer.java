fa4d8a1d612e9439945efeba7ed8d44d23e4ca4d
Florian Alpers <florian.alpers@intranda.com>
2018-03-07 14:53:38 +0100

    json/jackson serializers for user and comment

---
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/servlets/rest/content/CommentSerializer.java
@@ -0,0 +1,46 @@
+/**
+ * This file is part of the Goobi viewer - a content presentation and management application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.intranda.digiverso.presentation.servlets.rest.content;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import de.intranda.digiverso.presentation.model.annotation.Comment;
+
+/**
+ * @author Florian Alpers
+ *
+ */
+public class CommentSerializer extends JsonSerializer<Comment>{
+
+    /* (non-Javadoc)
+     * @see com.fasterxml.jackson.databind.JsonSerializer#serialize(java.lang.Object, com.fasterxml.jackson.core.JsonGenerator, com.fasterxml.jackson.databind.SerializerProvider)
+     */
+    @Override
+    public void serialize(Comment comment, JsonGenerator generator, SerializerProvider provider) throws IOException, JsonProcessingException {
+        generator.writeStartObject();
+        generator.writeStringField("format", "test/plain");
+        generator.writeStringField("type", "TextualBody");
+        generator.writeStringField("value", comment.getText());
+        generator.writeEndObject();
+        
+    }
+
+}
