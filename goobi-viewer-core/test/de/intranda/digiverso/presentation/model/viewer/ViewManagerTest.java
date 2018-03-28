74303d34619b7db274bf166c737d17d1d22de167
Florian Alpers <florian.alpers@intranda.com>
2018-03-28 09:11:05 +0200

    Added assertion failed message

--- goobi-viewer-core/test/de/intranda/digiverso/presentation/model/viewer/ViewManagerTest.java
+++ goobi-viewer-core/test/de/intranda/digiverso/presentation/model/viewer/ViewManagerTest.java
@@ -178,7 +178,8 @@ public class ViewManagerTest extends AbstractDatabaseAndSolrEnabledTest {
         viewManager.setFirstPdfPage("13");
         viewManager.setLastPdfPage("15");
         String url = viewManager.getPdfPartDownloadLink();
-        Assert.assertTrue(url.endsWith("image/PPN517154005/00000014.tif$00000015.tif$00000016.tif/full/max/0/PPN517154005_13-15.pdf"));
+        String expect = "image/PPN517154005/00000014.tif$00000015.tif$00000016.tif/full/max/0/PPN517154005_13-15.pdf";
+        Assert.assertTrue("expeted url to end with " + expect + " but was " + url, url.endsWith(expect));
 //                "?action=pdf&images=PPN517154005/00000014.tif$PPN517154005/00000015.tif$PPN517154005/00000016.tif&targetFileName=PPN517154005_13-15.pdf"));
     }
 }
