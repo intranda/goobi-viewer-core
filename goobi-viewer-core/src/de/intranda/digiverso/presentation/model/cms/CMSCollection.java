8b856ecda6b39127ce55e1173aba6a1a796f32d4
Florian Alpers <florian.alpers@intranda.com>
2018-10-30 13:37:40 +0100

    Match BrowseElements ignoring letter case

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/model/cms/CMSCollection.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/model/cms/CMSCollection.java
@@ -478,4 +478,5 @@ public class CMSCollection implements Comparable<CMSCollection>, BrowseElementIn
         }
     }
 
+
 }
--- goobi-viewer-core/src/de/intranda/digiverso/presentation/model/viewer/BrowseDcElement.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/model/viewer/BrowseDcElement.java
@@ -110,7 +110,7 @@ public class BrowseDcElement implements Comparable<BrowseDcElement>, Serializabl
             if (other.name != null) {
                 return false;
             }
-        } else if (!name.equals(other.name)) {
+        } else if (!name.equalsIgnoreCase(other.name)) {
             return false;
         }
         return true;
