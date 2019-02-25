71ce705f3a9e656de2075b18fc2a9e086fad6601
Florian Alpers <florian.alpers@intranda.com>
2019-02-25 16:00:48 +0100

    Added test for IIIF metadata config

---
+++ goobi-viewer-core/test/de/intranda/digiverso/presentation/model/iiif/presentation/builder/AbstractBuilderTest.java
@@ -0,0 +1,62 @@
+/**
+ * This file is part of the Goobi viewer - a content presentation and management application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.intranda.digiverso.presentation.model.iiif.presentation.builder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.intranda.digiverso.presentation.controller.Configuration;
+import de.intranda.digiverso.presentation.controller.DataManager;
+
+/**
+ * @author florian
+ *
+ */
+public class AbstractBuilderTest {
+
+	AbstractBuilder builder;
+	
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        DataManager.getInstance().injectConfiguration(new Configuration("resources/test/config_viewer.test.xml"));
+    }
+    
+    @Before
+    public void SetUp() {
+    	builder = new AbstractBuilder(URI.create("http://localhost:8080/viewer"), 
+        		URI.create("http://localhost:8080/viewer/rest/iiif/manifests/abcd/manifest")) {
+		};
+    }
+	
+	@Test
+	public void testGetEventFields() {
+		Map<String, List<String>> events = builder.getEventFields();
+		Assert.assertNotNull(events);
+		Assert.assertEquals(3, events.size());
+		Assert.assertEquals(2, events.get("").size());
+		Assert.assertEquals(2, events.get("Provenienz").size());
+		Assert.assertEquals(1, events.get("Expression Creation").size());
+		Assert.assertEquals("MD_EVENTARTIST", events.get("Expression Creation").iterator().next());
+
+	}
+
+}
