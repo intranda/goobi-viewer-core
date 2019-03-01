afc9affbb77a1a29ac05aecab1ee33abf5a06af6
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2019-03-01 16:59:58 +0100

    added validators for related PI checks (not yet working); refs #16977

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/faces/validators/RelatedPIValidator.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/faces/validators/RelatedPIValidator.java
@@ -22,49 +22,59 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-import de.intranda.digiverso.presentation.controller.DataManager;
+import org.apache.commons.lang.StringUtils;
+
 import de.intranda.digiverso.presentation.controller.Helper;
-import de.intranda.digiverso.presentation.exceptions.DAOException;
+import de.intranda.digiverso.presentation.managedbeans.UserBean;
 import de.intranda.digiverso.presentation.managedbeans.utils.BeanUtils;
 import de.intranda.digiverso.presentation.model.security.user.User;
 
 /**
- * Syntax validator for nicknames.
+ * Validates the entered PI belonging to a record for which the current user may create CMS content.
  */
-@FacesValidator("nicknameValidator")
-public class NicknameValidator implements Validator<String> {
+@FacesValidator("relatedPiValidator")
+public class RelatedPIValidator implements Validator<Object> {
+
+    private static final char[] ILLEGAL_CHARS = { '!', '?', '/', '\\', ':', ';', '(', ')', '@', '"', '\'' };
 
     /* (non-Javadoc)
      * @see javax.faces.validator.Validator#validate(javax.faces.context.FacesContext, javax.faces.component.UIComponent, java.lang.Object)
      */
     @Override
-    public void validate(FacesContext context, UIComponent component, String value) throws ValidatorException {
-        try {
-            if (!validateNicknameUniqueness(value)) {
-                FacesMessage msg = new FacesMessage(Helper.getTranslation("nickname_errExists", null), "");
-                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-                throw new ValidatorException(msg);
-            }
-        } catch (DAOException e) {
-            FacesMessage msg = new FacesMessage(Helper.getTranslation("login_internalerror", null), "");
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        User user = null;
+        UserBean userBean = BeanUtils.getUserBean();
+        if (userBean != null) {
+            user = userBean.getUser();
+        }
+        if (!validatePi((String) value, user)) {
+            FacesMessage msg = new FacesMessage(Helper.getTranslation("pi_errInvalid", null), "");
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(msg);
         }
     }
 
     /**
-     * @param email
-     * @return true if the given email is not already assigned to a user except a possibly currently logged in user in this session
-     * @throws DAOException
+     *
+     * @param pi
+     * @param user
+     * @return
+     * @should return true if pi good
+     * @should return false if pi empty, blank or null
+     * @should return false if user is null
+     * @should return true if user is superuser
      */
-    private static boolean validateNicknameUniqueness(String nick) throws DAOException {
-        User user = DataManager.getInstance().getDao().getUserByNickname(nick);
-        User currentUser = BeanUtils.getUserBean().getUser();
-        if (user != null) {
-            return currentUser != null && user.getId().equals(currentUser.getId());
+    public static boolean validatePi(String pi, User user) {
+        if (StringUtils.isBlank(pi)) {
+            return false;
+        }
+        if (user == null || !user.isCmsAdmin()) {
+            return false;
+        }
+        if (user.isSuperuser()) {
+            return true;
         }
 
-        return true;
+        return !StringUtils.containsAny(pi, ILLEGAL_CHARS);
     }
-
 }
