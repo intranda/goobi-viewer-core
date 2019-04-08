5e0ffc4bf7c8e14155f158ed8e55db6a5ed4eb60
Florian Alpers <florian.alpers@intranda.com>
2019-04-08 17:22:04 +0200

    Moved download rest resource to rest package

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/servlets/rest/download/DownloadBinding.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/servlets/rest/download/DownloadBinding.java
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package de.intranda.digiverso.presentation.servlets.download;
+package de.intranda.digiverso.presentation.servlets.rest.download;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
