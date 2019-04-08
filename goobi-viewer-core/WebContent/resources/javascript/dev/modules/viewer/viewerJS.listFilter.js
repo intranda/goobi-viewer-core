10db76bdcb74f0ea41f4cf97ff0af4a740963c49
Florian Alpers <florian.alpers@intranda.com>
2019-04-08 16:26:58 +0200

    removed debugging in listFilter

--- goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewer/viewerJS.listFilter.js
+++ goobi-viewer-core/WebContent/resources/javascript/dev/modules/viewer/viewerJS.listFilter.js
@@ -33,7 +33,7 @@
 var viewerJS = ( function( viewer ) {
     'use strict';
     
-    var _debug = true;
+    var _debug = false;
     
     viewer.listFilter = function(config) {
         if ( _debug ) {
