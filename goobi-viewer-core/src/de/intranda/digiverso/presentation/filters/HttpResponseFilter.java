0c9da5457ad80017cd88be11a72e7d21d38a3348
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2019-05-09 13:50:48 +0200

    removed unused imports with missing dependencies

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/filters/HttpResponseFilter.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/filters/HttpResponseFilter.java
@@ -31,8 +31,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.meterware.pseudoserver.HttpRequest;
-
 import de.intranda.digiverso.presentation.controller.DataManager;
 
 /**
