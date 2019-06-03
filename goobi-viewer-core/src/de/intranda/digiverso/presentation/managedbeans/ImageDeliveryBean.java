14157180623b253c2264070ccd4ac081e5e50653
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2019-06-03 17:42:16 +0200

    Revert "log info instead of error if servlet path couldn't be retrieved via servlet request or jsf context"
    This reverts commit c13d672c199ae0795c9367309aedf955eafb156a.

--- goobi-viewer-core/src/de/intranda/digiverso/presentation/managedbeans/ImageDeliveryBean.java
+++ goobi-viewer-core/src/de/intranda/digiverso/presentation/managedbeans/ImageDeliveryBean.java
@@ -106,7 +106,7 @@ public class ImageDeliveryBean implements Serializable {
             } else if (BeanUtils.hasJsfContext()) {
                 this.servletPath = BeanUtils.getServletPathWithHostAsUrlFromJsfContext();
             } else {
-                logger.info("Failed to initialize ImageDeliveryBean: No servlet request and no JSF context found");
+                logger.error("Failed to initialize ImageDeliveryBean: No servlet request and no jsf context found");
                 servletPath = "";
             }
             init(config, servletPath);
@@ -379,7 +379,7 @@ public class ImageDeliveryBean implements Serializable {
     public MediaHandler getMedia() {
         return media;
     }
-
+    
     public Object3DHandler getObjects3D() {
         return objects3d;
     }
@@ -511,5 +511,6 @@ public class ImageDeliveryBean implements Serializable {
     public Optional<String> getIfExists(String url) {
         return Optional.of(url).map(string -> StringUtils.isNotBlank(string) ? string : null);
     }
+
 
 }
