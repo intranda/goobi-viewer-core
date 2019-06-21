baf102fe295737a7ed882b5557b985e003231a50
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2019-06-21 14:39:54 +0200

    add authority link as NORM_PROVIDER; added messages for new NORM_ keys; refs #17266

--- goobi-viewer-core/src/io/goobi/viewer/servlets/rest/content/NormdataResource.java
+++ goobi-viewer-core/src/io/goobi/viewer/servlets/rest/content/NormdataResource.java
@@ -157,7 +157,7 @@ public class NormdataResource {
             // Authority URL
             NormDataValue authorityUrl = MarcRecord.getAuthorityUrlFromViafUrl(url);
             authorityUrl.setImageFileName("resources/images/authority/" + authorityUrl.getLabel() + ".png");
-            normDataList.add(new NormData("NORM_AUTHORITY_" + authorityUrl.getLabel(), authorityUrl));
+            normDataList.add(new NormData("NORM_PROVIDER", authorityUrl));
         }
 
         JSONArray jsonArray = new JSONArray();
