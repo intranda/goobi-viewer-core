be45295f51391b342f3affdcbd909ffa617ee419
Florian Alpers <florian.alpers@intranda.com>
2019-09-20 10:23:52 +0200

    Mark locations on map

--- goobi-viewer-core/src/META-INF/resources/resources/javascript/dist/riot-tags.js
+++ goobi-viewer-core/src/META-INF/resources/resources/javascript/dist/riot-tags.js
@@ -717,22 +717,62 @@ riot.tag2('fsthumbnails', '<div class="fullscreen__view-image-thumbs" ref="thumb
     		this.update();
     	}.bind( this ) );
 });
-riot.tag2('geolocationquestion', '<div if="{this.showInstructions()}" class="annotation_instruction"><label>Halten sie die Shift-Taste gedr&#x00FCckt und ziehen Sie im Bild einen Bereich mit der Maus auf.</label></div><div id="geoMap"></div><div id="annotation_{index}" each="{anno, index in this.annotations}"></div>', '', '', function(opts) {
+riot.tag2('geolocationquestion', '<div if="{this.showInstructions()}" class="annotation_instruction"><label>Halten sie die Shift-Taste gedr&#x00FCckt und ziehen Sie im Bild einen Bereich mit der Maus auf.</label></div><div id="geoMap" class="geo-map"></div><div id="annotation_{index}" each="{anno, index in this.annotations}"></div>', '', '', function(opts) {
 
-on("mount", function() {
+this.question = this.opts.question;
+this.question.createAnnotation = function(anno) {
+    let annotation = new Crowdsourcing.Annotation.GeoJson(anno);
+    annotation.generator = this.question.getGenerator();
+    annotation.creator = this.opts.item.getCreator();
+    return annotation;
+}.bind(this);
+
+this.on("mount", function() {
     this.initMap();
 })
 
 this.initMap = function() {
-    var map = new L.Map('geoMap');
+    this.map = new L.Map('geoMap');
     var osm = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       minZoom: 0,
       maxZoom: 20,
       attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     });
 
-    map.setView(new L.LatLng(49.451993, 11.073397), 5);
-    map.addLayer(osm);
+    this.map.setView(new L.LatLng(49.451993, 11.073397), 5);
+    this.map.addLayer(osm);
+
+    this.locations = L.geoJSON([], {
+        style : {
+                "color": "#ff7800",
+            	"weight": 5,
+            	"opacity": 0.65
+        }
+    }).addTo(this.map);
+
+    this.map.on("click", function(e) {
+        var location= e.latlng;
+        this.addFeature(location);
+    }.bind(this))
+
+}.bind(this)
+
+this.addFeature = function(location) {
+
+    var geojsonFeature = {
+        	"type": "Feature",
+        	"properties": {
+        		"name": "Coors Field",
+        		"amenity": "Baseball Stadium",
+        		"popupContent": "This is where the Rockies play!"
+        	},
+        	"geometry": {
+        		"type": "Point",
+        		"coordinates": [location.lng, location.lat]
+        	}
+        };
+    console.log("add feature ", geojsonFeature ),
+    this.locations.addData(geojsonFeature);
 }.bind(this)
 
 this.showInstructions = function() {
