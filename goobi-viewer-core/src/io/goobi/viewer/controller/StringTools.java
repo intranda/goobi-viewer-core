218ab7bb309525f47d56e5cf8f8e7c09edd766fc
Jan Vonde <mail@jan-von.de>
2019-12-27 20:57:37 +0100

    fixes deprecation message for org.apache.commons.lang3.StringEscapeUtils
    the class moved to org.apache.commons.text

--- goobi-viewer-core/src/io/goobi/viewer/controller/StringTools.java
+++ goobi-viewer-core/src/io/goobi/viewer/controller/StringTools.java
@@ -37,7 +37,7 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- goobi-viewer-core/src/io/goobi/viewer/managedbeans/NavigationHelper.java
+++ goobi-viewer-core/src/io/goobi/viewer/managedbeans/NavigationHelper.java
@@ -43,7 +43,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
