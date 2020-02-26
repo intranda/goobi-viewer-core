41a3a64232c8a7a6a4d334e1cce862341a1e9e0d
Florian Alpers <florian.alpers@intranda.com>
2020-02-26 15:01:04 +0100

    Additional check in SearchResultConverterTest.testGetAnnotationsFromAlto

--- goobi-viewer-core/src/test/java/io/goobi/viewer/model/iiif/search/SearchResultConverterTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/iiif/search/SearchResultConverterTest.java
@@ -19,6 +19,7 @@ import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.common.SolrDocument;
 import org.junit.After;
 import org.junit.Assert;
@@ -165,6 +166,7 @@ public class SearchResultConverterTest extends AbstractSolrEnabledTest {
 
         Assert.assertNotNull("Converter is null", converter);
         Assert.assertNotNull("Alto doc is null", doc);
+        Assert.assertTrue("Query regex is Blank", StringUtils.isNotBlank(queryRegex));
 
         AnnotationResultList results = converter.getAnnotationsFromAlto(doc, queryRegex);
         Assert.assertEquals(9, results.hits.size());
