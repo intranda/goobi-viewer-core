fa1cf1213ed804539e84190c685536eed1a63721
Florian Alpers <florian.alpers@intranda.com>
2020-04-22 16:28:10 +0200

    updated derived files

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dist/riot-tags.js
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dist/riot-tags.js
@@ -1644,6 +1644,74 @@ riot.tag2('imageview', '<div id="wrapper_{opts.id}" class="imageview_wrapper"><s
 });
 
 
+
+riot.tag2('metadataeditor', '<div if="{this.metadataList && this.metadataList.length > 0}"><ul class="nav nav-tabs"><li each="{language, index in this.opts.languages}" class="{language == this.currentLanguage ? \'active\' : \'\'}"><a onclick="{this.setCurrentLanguage}">{language}</a></li></ul><div class="tab-content"><div class="tab-pane active"><div class="input_form"><div each="{metadata, index in this.metadataList}" class="input_form__option_group"><div class="input_form__option_label"><label for="input-{metadata.property}">{metadata.label}:</label></div><div class="input_form__option_marker {metadata.required ? \'in\' : \'\'}"><label>*</label></div><div class="input_form__option_control"><input ref="input" if="{metadata.type != \'longtext\'}" type="{metadata.type}" id="input-{metadata.property}" class="form-control" riot-value="{getValue(metadata)}" oninput="{this.updateMetadata}"><textarea ref="input" if="{metadata.type == \'longtext\'}" id="input-{metadata.property}" class="form-control" riot-value="{getValue(metadata)}" oninput="{this.updateMetadata}"></textarea></div><div if="{metadata.helptext}" class="input_form__option_help"><button type="button" class="btn btn--clean" data-toggle="helptext" for="help_{metadata.property}"><i class="fa fa-question-circle" aria-hidden="true"></i></button></div><div if="{metadata.helptext}" id="help_{metadata.property}" class="input_form__option_control_helptext">{metadata.helptext}</div></div></div></div></div></div>', '', '', function(opts) {
+
+ 	this.on("mount", () => {
+ 	    console.log("mount metadataEditor ", this.opts);
+ 	    this.currentLanguage = this.opts.currentLanguage;
+ 	    this.updateMetadataList(this.opts.metadata);
+ 	    this.focusInput();
+ 	    if(this.opts.provider) {
+ 	        this.opts.provider.subscribe( (metadata) => {
+ 	            this.updateMetadataList(metadata)
+ 	            this.update();
+ 	            this.focusInput();
+ 	        });
+ 	    }
+ 	})
+
+ 	this.focusInput = function() {
+ 	    if(Array.isArray(this.refs.input)) {
+ 	        this.refs.input[0].focus();
+ 	    } else if(this.refs.input) {
+ 	        this.refs.input.focus();
+ 	    }
+ 	}.bind(this)
+
+ 	this.updateMetadataList = function(metadataList) {
+ 	   this.metadataList = metadataList;
+       	this.metadataList.forEach(md => {
+   	        let valueObject = this.getValueForLanguages(this.opts.languages);
+   	        if(typeof md.value == "string") {
+   	            valueObject[this.currentLanguage] = [md.value];
+   	        } else if(md.value) {
+   	            $.extend(valueObject, md.value);
+   	        }
+   	        md.value = valueObject;
+   	    })
+ 	}.bind(this)
+
+ 	this.getValueForLanguages = function(languages) {
+ 	    let ret = {};
+ 	    languages.forEach(lang => {
+ 	        ret[lang] = []
+ 	    })
+ 	    return ret;
+ 	}.bind(this)
+
+ 	this.updateMetadata = function(event) {
+ 	    let metadata = event.item.metadata;
+ 	    metadata.value[this.currentLanguage] = [event.target.value];
+ 	    if(metadata.listener) {
+ 	        metadata.listener.next(metadata);
+ 	    }
+ 	}.bind(this)
+
+ 	this.getValue = function(metadata) {
+ 	    let value = viewerJS.getMetadataValue(metadata.value, this.currentLanguage);
+ 	    return value;
+ 	}.bind(this)
+
+ 	this.setCurrentLanguage = function(event) {
+ 	    this.currentLanguage = event.item.language;
+ 	    this.update();
+ 	}.bind(this)
+
+});
+
+
+
 riot.tag2('pdfdocument', '<div class="pdf-container"><pdfpage each="{page, index in pages}" page="{page}" pageno="{index+1}"></pdfPage></div>', '', '', function(opts) {
 
 		this.pages = [];
