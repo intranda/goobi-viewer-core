824aae8954f721cb2a3d8bcd63a1902ac219e860
Florian Alpers <florian.alpers@intranda.com>
2020-07-02 22:18:27 +0200

    only add slash after rest base url if neccessary

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/imaging/IIIFUrlHandler.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/imaging/IIIFUrlHandler.java
@@ -113,7 +113,10 @@ public class IIIFUrlHandler {
                     return IIIFUrlResolver.getModifiedIIIFFUrl(fileUrl, region, size, rotation, quality, format);
                 } else if (ImageHandler.isImageUrl(fileUrl, false)) {
                     StringBuilder sb = new StringBuilder(apiUrl);
-                    sb.append("/image/-/").append(BeanUtils.escapeCriticalUrlChracters(fileUrl, true)).append("/");
+                    if(!apiUrl.endsWith("/")) {
+                        sb.append("/");
+                    }
+                    sb.append("image/-/").append(BeanUtils.escapeCriticalUrlChracters(fileUrl, true)).append("/");
                     sb.append(region).append("/");
                     sb.append(size).append("/");
                     sb.append(rotation).append("/");
