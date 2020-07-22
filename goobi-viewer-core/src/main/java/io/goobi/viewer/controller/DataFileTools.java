a2fb0f92425302834f62ed9505d94de45ecc0451
Florian Alpers <florian.alpers@intranda.com>
2020-07-22 10:36:58 +0200

    Don't allow filepaths as Filename when requesting source files

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/DataFileTools.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/DataFileTools.java
@@ -186,6 +186,8 @@ public class DataFileTools {
      */
     public static Path getDataFilePath(String pi, String dataFolderName, String altDataFolderName, String fileName)
             throws PresentationException, IndexUnreachableException {
+        //make sure fileName is a pure filename and not a path
+        fileName = Paths.get(fileName).getFileName().toString();
         java.nio.file.Path dataFolderPath = getDataFolder(pi, dataFolderName);
         if (StringUtils.isNotBlank(fileName)) {
             dataFolderPath = dataFolderPath.resolve(fileName);
