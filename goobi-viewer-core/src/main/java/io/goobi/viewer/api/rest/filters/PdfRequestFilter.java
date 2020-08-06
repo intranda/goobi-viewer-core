1de5af49624c3140aa8964cd8f6529c36e111d5b
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-08-06 10:19:15 +0200

    check quota for pages and chapters; refs #19325

--- goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/filters/PdfRequestFilter.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/filters/PdfRequestFilter.java
@@ -18,6 +18,12 @@ package io.goobi.viewer.api.rest.filters;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -29,6 +35,8 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +44,8 @@ import de.unigoettingen.sub.commons.contentlib.exceptions.ServiceNotAllowedExcep
 import de.unigoettingen.sub.commons.contentlib.servlet.rest.ContentExceptionMapper.ErrorMessage;
 import de.unigoettingen.sub.commons.contentlib.servlet.rest.ContentServerPdfBinding;
 import io.goobi.viewer.controller.DataManager;
+import io.goobi.viewer.controller.SolrConstants;
+import io.goobi.viewer.controller.SolrSearchIndex;
 import io.goobi.viewer.controller.StringTools;
 import io.goobi.viewer.exceptions.DAOException;
 import io.goobi.viewer.exceptions.IndexUnreachableException;
@@ -43,6 +53,7 @@ import io.goobi.viewer.exceptions.PresentationException;
 import io.goobi.viewer.exceptions.RecordNotFoundException;
 import io.goobi.viewer.model.security.AccessConditionUtils;
 import io.goobi.viewer.model.security.IPrivilegeHolder;
+import io.goobi.viewer.model.viewer.StringPair;
 
 /**
  * <p>
@@ -54,6 +65,8 @@ import io.goobi.viewer.model.security.IPrivilegeHolder;
 public class PdfRequestFilter implements ContainerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(PdfRequestFilter.class);
+
+    private static final String ATTRIBUTE_PDF_QUOTA = "pdf_quota";
 
     @Context
     private HttpServletRequest servletRequest;
@@ -119,24 +132,127 @@ public class PdfRequestFilter implements ContainerRequestFilter {
     void filterForDownloadQuota(String pi, String divId, String contentFileName, HttpServletRequest request)
             throws ServiceNotAllowedException {
         try {
-            int quota = AccessConditionUtils.getPdfDownloadQuotaForRecord(pi);
+            int percentage = AccessConditionUtils.getPdfDownloadQuotaForRecord(pi);
+
+            // Full record PDF
             if (StringUtils.isEmpty(divId) && StringUtils.isEmpty(contentFileName)) {
-                // Full record PDF
-                if (quota < 100) {
-                    throw new ServiceNotAllowedException("Insufficient download quota for record '" + pi + "': " + quota + "%");
+                if (percentage < 100) {
+                    throw new ServiceNotAllowedException("Insufficient download quota for record '" + pi + "': " + percentage + "%");
                 }
-            } else if (StringUtils.isNotEmpty(divId) && StringUtils.isEmpty(contentFileName)) {
+                return;
+            }
+
+            int numTotalRecordPages = (int) DataManager.getInstance()
+                    .getSearchIndex()
+                    .getHitCount("+" + SolrConstants.PI_TOPSTRUCT + " +" + SolrConstants.DOCTYPE + ":PAGE");
+
+            if (StringUtils.isNotEmpty(divId) && StringUtils.isEmpty(contentFileName)) {
                 // Chapter PDF
-                // TODO check session storage
+                SolrDocumentList docs = DataManager.getInstance()
+                        .getSearchIndex()
+                        .search(
+                                "+" + SolrConstants.PI_TOPSTRUCT + " +" + SolrConstants.LOGID + ":" + divId + " +" + SolrConstants.DOCTYPE + ":PAGE",
+                                SolrSearchIndex.MAX_HITS, Collections.singletonList(new StringPair(SolrConstants.ORDER, "asc")),
+                                Arrays.asList(new String[] { SolrConstants.ORDER, SolrConstants.FILENAME }));
+
+                if (docs.isEmpty()) {
+                    throw new RecordNotFoundException("Document not found: " + pi + "/" + divId);
+                }
+                // Check each page that belongs to the requested docstruct
+                for (SolrDocument doc : docs) {
+                    String fileName = (String) doc.getFieldValue(SolrConstants.FILENAME);
+                    if (StringUtils.isEmpty(fileName)) {
+                        logger.error("File name not found for page belonging to {}/{}", pi, divId);
+                    }
+                    if (!checkPageAllowed(pi, fileName, percentage, numTotalRecordPages, request)) {
+                        throw new ServiceNotAllowedException("Insufficient download quota for record '" + pi + "': " + percentage + "%");
+                    }
+                }
             } else if (StringUtils.isEmpty(divId) && StringUtils.isNotEmpty(contentFileName)) {
                 // Page PDF
-                // TODO check session storage
+                if (!checkPageAllowed(pi, contentFileName, percentage, numTotalRecordPages, request)) {
+                    throw new ServiceNotAllowedException("Insufficient download quota for record '" + pi + "': " + percentage + "%");
+                }
             }
-        } catch (PresentationException | IndexUnreachableException | DAOException |
-
-                RecordNotFoundException e) {
+        } catch (PresentationException | IndexUnreachableException | DAOException | RecordNotFoundException e) {
             throw new ServiceNotAllowedException(e.getMessage());
         }
+    }
+
+    /**
+     * 
+     * @param pi Record identifier
+     * @param pageFile Page file name
+     * @param percentage Allowed percentage of pages for PDF download
+     * @param numTotalRecordPages
+     * @param request HTTP servlet request object
+     * @return true if page allowed as part of the quota; false otherwise
+     * @should return false if session unavailable
+     * @should return false if no session attribute exists yet
+     * @should return true if page already part of quota
+     * @should return false if quota already filled
+     * @should return true and add page to map if quota not yet filled
+     */
+    @SuppressWarnings("unchecked")
+    static boolean checkPageAllowed(String pi, String pageFile, int percentage, int numTotalRecordPages, HttpServletRequest request) {
+        if (request == null || request.getSession() == null) {
+            return false;
+        }
+        try {
+            Map<String, Set<String>> quotaMap = (Map<String, Set<String>>) request.getSession().getAttribute(ATTRIBUTE_PDF_QUOTA);
+            if (quotaMap == null) {
+                request.getSession().setAttribute(ATTRIBUTE_PDF_QUOTA, new HashMap<String, Set<String>>());
+                return false;
+            }
+            if (quotaMap.get(pi) == null) {
+                quotaMap.put(pi, new HashSet<>());
+            }
+            // Page already allowed as part of the quota
+            if (quotaMap.get(pi).contains(pageFile)) {
+                return true;
+            }
+            // Quota already filled and requested page is not part of it
+            int allowedPages = getNumAllowedPages(percentage, numTotalRecordPages);
+            if (quotaMap.get(pi).size() >= allowedPages) {
+                return false;
+            }
+            // Add file to quotas
+            quotaMap.get(pi).add(pageFile);
+            return true;
+        } catch (ClassCastException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return false;
+    }
+
+    /**
+     * Calculates the maximum number of allowed pages from the total number of pages for a record and the given percentage.
+     * 
+     * @param percentage Allowed percentage for downloads
+     * @param numTotalRecordPages Total record pages
+     * @return Maximum number of pages for the given percentage
+     * @should return 0 if percentage 0
+     * @should return 0 if number of pages 0
+     * @should return number of pages if percentage 100
+     * @should calculate number correctly
+     * 
+     */
+    static int getNumAllowedPages(int percentage, int numTotalRecordPages) {
+        if (percentage < 0) {
+            throw new IllegalArgumentException("percentage may not be less than 0");
+        }
+        if (numTotalRecordPages < 0) {
+            throw new IllegalArgumentException("numTotalRecordPages may not be less than 0");
+        }
+        if (numTotalRecordPages == 0 || percentage == 0) {
+            return 0;
+        }
+        if (percentage == 100) {
+            return numTotalRecordPages;
+        }
+
+        return (int) Math.floor(((double) numTotalRecordPages / 100 * percentage));
     }
 
     /**
