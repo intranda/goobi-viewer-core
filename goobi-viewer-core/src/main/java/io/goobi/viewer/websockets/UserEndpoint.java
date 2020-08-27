657b13880b5b5ffde5dd7e7523bb0af23f93a81c
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-08-27 14:13:28 +0200

    added socket endpoint; refs #19324

---
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/websockets/UserEndpoint.java
@@ -0,0 +1,63 @@
+/**
+ * This file is part of the Goobi viewer - a content presentation and management application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.goobi.viewer.websockets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.goobi.viewer.model.security.user.User;
+
+@ServerEndpoint(value = "/socket/session")
+public class UserEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserEndpoint.class);
+
+    private static Map<String, Set<User>> locks = new HashMap<>();
+
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+        logger.trace("onOpen: {}", session.getId());
+        this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        logger.trace("onClose {}", session.getId());
+    }
+
+    @OnError
+    public void onError(Session session, Throwable t) {
+        logger.error(t.getMessage());
+    }
+}
