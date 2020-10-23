b6fd1a23970857fe68e0e490fd9a06de2ffc01eb
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-10-23 13:44:13 +0200

    updated user annotations page url; refs #19338

--- goobi-viewer-core/src/main/java/io/goobi/viewer/filters/LoginFilter.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/filters/LoginFilter.java
@@ -151,7 +151,6 @@ public class LoginFilter implements Filter {
         logger.trace("uri: {}", uri);
         switch (uri.trim()) {
             case "/myactivity/":
-            case "/myannotations/":
             case "/mysearches/":
                 return true;
             default:
