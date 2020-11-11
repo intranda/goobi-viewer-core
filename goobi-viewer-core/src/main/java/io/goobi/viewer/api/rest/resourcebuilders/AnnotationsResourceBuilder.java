0e71be58fcabecb58b581a60dbaba926302892a0
Florian Alpers <florian.alpers@intranda.com>
2020-11-11 15:06:58 +0100

    Only write rights to webannotations when indxing

--- goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/resourcebuilders/AnnotationsResourceBuilder.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/resourcebuilders/AnnotationsResourceBuilder.java
@@ -445,7 +445,6 @@ public class AnnotationsResourceBuilder {
             annotation.setBody(getBodyAsResource(anno));
             annotation.setTarget(getTargetAsResource(anno));
             annotation.setMotivation(anno.getMotivation());
-            annotation.setRights(anno.getAccessCondition());
         } catch (IOException e) {
             logger.error("Error creating web annotation from " + anno, e);
         }
