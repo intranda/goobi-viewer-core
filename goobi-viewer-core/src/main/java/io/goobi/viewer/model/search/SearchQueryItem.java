6c74f0c4c673c6ad9d665359b416a5d7176f6a0b
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2020-12-21 13:48:52 +0100

    pass "en" as default language

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/search/SearchQueryItem.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/search/SearchQueryItem.java
@@ -115,7 +115,7 @@ public class SearchQueryItem implements Serializable {
             return getSelectItems(locale.getLanguage());
         }
 
-        return getSelectItems(null);
+        return getSelectItems("en");
     }
 
     /**
