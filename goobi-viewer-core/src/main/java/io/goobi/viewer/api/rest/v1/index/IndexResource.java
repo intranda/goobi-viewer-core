2c7f6f974a893cc69da820804db39cbc95f6b702
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2021-02-19 13:36:01 +0100

    updated @Operation; refs #19447

--- goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/v1/index/IndexResource.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/v1/index/IndexResource.java
@@ -274,7 +274,7 @@ public class IndexResource {
     @GET
     @Path(INDEX_FIELDS)
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Requires an authentication token. Retrieves a JSON list of all existing Solr fields.", tags = { "solr" })
+    @Operation(summary = "Retrieves a JSON list of all existing Solr fields.", tags = { "index" })
     public List<SolrFieldInfo> getAllIndexFields() throws IOException {
         logger.trace("getAllIndexFields");
 
