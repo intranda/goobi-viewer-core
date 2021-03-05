a587431f49bad9ff5f8b502e56b65883c0a2cc21
Florian Alpers <florian.alpers@intranda.com>
2021-03-05 08:08:24 +0100

    Display UGC of page below image Add config for above feature
    refs #20339

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/Configuration.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/Configuration.java
@@ -4973,4 +4973,8 @@ public final class Configuration extends AbstractConfiguration {
     public HierarchicalConfiguration getBaseXMetadataConfig() {
         return getLocalConfigurationAt("metadata.basexMetadataList");
     }
+    
+    public boolean isDisplayUserGeneratedContentBelowImage() {
+        return getLocalBoolean("webGuiDisplay.displayUserGeneratedContentBelowImage", false);
+    }
 }
