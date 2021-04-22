7992f92679d04dee936160f90978e94c6368cc97
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2021-04-22 15:47:08 +0200

    added TranslationGroup.equals() to fix List.indexOf() returning -1; refs #20355

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/translations/admin/TranslationGroup.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/translations/admin/TranslationGroup.java
@@ -113,6 +113,34 @@ public class TranslationGroup {
         this.items = new ArrayList<>(numKeys);
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TranslationGroup other = (TranslationGroup) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
     /**
      * @return the id
      */
