e01f63cb46a98a07d681f29945bb5cef81fe434a
Florian Alpers <florian.alpers@intranda.com>
2021-06-15 12:03:56 +0200

    Moved rss feed resources Fixed rssFeed imports

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/libs/rssFeedLib/get_rss_feed.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$sUrl = (isset($_GET['url']) && $_GET['url'] != '') ? $_GET['url'] : 'http://www.boonex.com/unity/extensions/latest/?rss=1';
-
-header( 'Content-Type: text/xml' );
-readfile($sUrl);
-
-?>
