4964bc5ff5069056eafba73f07b84676ba567e7d
Florian Alpers <florian.alpers@intranda.com>
2021-06-23 09:33:04 +0200

    Logging for API tasks

--- goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/v1/tasks/TasksResource.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/api/rest/v1/tasks/TasksResource.java
@@ -34,6 +34,9 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.unigoettingen.sub.commons.contentlib.exceptions.ContentNotFoundException;
 import de.unigoettingen.sub.commons.contentlib.exceptions.IllegalRequestException;
 import io.goobi.viewer.api.rest.filters.AdminLoggedInFilter;
@@ -57,8 +60,10 @@ import io.swagger.v3.oas.annotations.Parameter;
 @Path(TASKS)
 public class TasksResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(TasksResource.class);
     private final HttpServletRequest request;
 
+    
     public TasksResource(@Context HttpServletRequest request, @Context HttpServletResponse response) {
         this.request = request;
     }
@@ -73,6 +78,7 @@ public class TasksResource {
         }
         if (isAuthorized(desc.type, Optional.empty(), request)) {
             Task job = new Task(desc, TaskManager.createTask(desc.type));
+            logger.debug("Created new task REST API task '{}'", job);
             DataManager.getInstance().getRestApiJobManager().addTask(job);
             DataManager.getInstance().getRestApiJobManager().triggerTaskInThread(job.id, request);
             return job;
