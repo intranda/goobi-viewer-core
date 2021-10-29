d15c371ccc06fe82e88ed1320fdbf3086913f57c
Florian Alpers <florian.alpers@intranda.com>
2021-10-29 16:08:17 +0200

    Fixed creating custom sidebar elements in cms-page editor

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/CmsBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/CmsBean.java
@@ -1962,6 +1962,7 @@ public class CmsBean implements Serializable {
         } else {
             validateSidebarElement(this.selectedSidebarElement);
             this.selectedPage.addSidebarElement(this.selectedSidebarElement);
+            this.selectedSidebarElement = null;
         }
     }
 
