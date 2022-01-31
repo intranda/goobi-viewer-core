199e8aa2987c3e207c4acc181d575560aa8485e1
Jan Günther <jan.guenther@intranda.com>
2022-01-31 14:08:15 +0100

    changed placement of script admin tables no entry message refs #22191

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/admin/adminJS.translationsEdit.js
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/admin/adminJS.translationsEdit.js
@@ -138,7 +138,9 @@ var adminJS = ( function( admin ) {
     			
     			if($('.admin__table-entry').length == 0) {
     				$('.admin__table-content').append('<br/><p >#{msg.hitsZero}</p>');
+					console.log('javascript changed');
     			}
+
     			
     			$('.admin__translations-search-input').keyup(function() {
         	        if ($('.admin__translations-search-input').val() == ''){
