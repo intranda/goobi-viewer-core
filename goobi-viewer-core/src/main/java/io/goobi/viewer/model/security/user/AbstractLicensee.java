cda5991cc2624a190a3087d5e013598e8aa2742f
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2022-08-11 13:59:20 +0200

    some unit tests; refs #19525

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/security/user/AbstractLicensee.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/security/user/AbstractLicensee.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.goobi.viewer.model.security.user;
 
 import java.util.Collections;
@@ -69,10 +90,14 @@ public abstract class AbstractLicensee implements ILicensee {
 
     /**
      * 
-     * @param permissionMap
-     * @return
+     * @param permissionMap Map containing permission check outcome for different license names
+     * @return {@link AccessPermission}
+     * @should return denied if permissionMap empty
+     * @should return denied if all permissions in map denied
+     * @should preserve ticketRequired
+     * @should preserve redirect metadata
      */
-    public AccessPermission getAccessPermissionFromMap(Map<String, AccessPermission> permissionMap) {
+    public static AccessPermission getAccessPermissionFromMap(Map<String, AccessPermission> permissionMap) {
         // It should be sufficient if the user can satisfy one required license
         boolean granted = false;
         boolean ticketRequired = false;
