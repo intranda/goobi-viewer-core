5ee7cf7fd39693ea7ab55dca12cbba6231447740
Jan Günther <jan.guenther@intranda.com>
2022-08-12 08:07:19 +0200

    basic structures (+tests) refs #22348

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/admin/adminJS.js
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/admin/adminJS.js
@@ -145,3 +145,129 @@ $( document ).ready(function() {
 
 // END DOCUMENT READY
 });
+
+
+
+
+console.log("index.js invoked");
+
+var configFileTextArea;
+var type;
+var configFileEditor;
+var readOnly;
+var selected_row;
+//var nightMode;
+var theme;
+  	 
+/*	
+if (readOnly === undefined) {
+	readOnly = true;
+}
+*/ 	 
+//initTextArea();
+					 
+//var table = document.getElementById("file-form:file-table");
+//var rows = document.getElementsByTagName("tr");
+//	
+//for (let i = 0; i < rows.length; ++i) {
+//	var currentRow = table.rows[i];
+//	currentRow.setAttribute('id', 'row'+i);
+//	currentRow.onclick = function() {
+//	/*
+//		Array.from(this.parentElement.children).forEach(function(el){
+//			el.classList.remove('selected-row');
+//		});
+//	*/ 
+//	[...this.parentElement.children].forEach((el) => el.classList.remove("selected-row"));
+//	this.classList.add('selected-row');
+//	};
+//}
+
+function initTextArea() {
+	let fileTypeElement = document.getElementById("currentConfigFileType");
+	console.log("fileTypeElement = " + fileTypeElement);
+	if (fileTypeElement !== null){
+		type = fileTypeElement.innerHTML.trim(); // "properties" or "xml"
+	} else {
+		type = "xml";
+	}
+	console.log("type = " + type);
+//	nightMode = document.getElementById("nightMode").innerHTML.trim(); // "true" or "false"
+	if (typeof type == "undefined") {
+		type = "xml";
+	}
+	if (typeof theme == "undefined") {
+		theme = "default";
+	}
+//	
+//	if (nightMode == "true") {
+//		theme = "blackboard";
+//	} else {
+//		theme = "default";
+//	}
+//
+	configFileTextArea = document.getElementById("editor-form:editor");
+
+	if (configFileTextArea !== null) {
+		if (configFileEditor) {
+			configFileEditor.toTextArea();
+//			configFileEditor = null;
+			console.log("CodeMirror Editor freed!"); 
+		}
+		configFileEditor = CodeMirror.fromTextArea(configFileTextArea, {
+			lineNumbers: true,
+			mode: type,
+			theme: theme,
+			readOnly: readOnly,
+			autofocus: true,
+			indextUnit: 4,
+			extraKeys: {
+				"F11": function(cm) {
+					cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+				},
+				"Esc": function(cm) {
+					if (cm.getOption("fullScreen")) {
+						cm.setOption("fullScreen", false);
+					}
+				},
+				"Ctrl-D": function(cm) {
+					cm.setOption("theme", cm.getOption("theme") == "default" ? "dracula" : "default");
+				}
+			}
+			
+		}); 
+		console.log("CodeMirror Editor constructed!");
+//		configFileEditor.focus();
+/*
+		setTimeout(function(){
+			configFileEditor.refresh();	
+		}, 100);
+*/ 
+	}
+ 
+}
+
+function setEditable(editable, number, isButton) {
+	selected_row = document.getElementById('row'+number);
+	
+	readOnly = !editable;
+	if (readOnly && isButton) {
+		alert("Die Rechte im Dateisystem müssen korrigiert werden um diese Datei zu bearbeiten.");
+	}
+}
+
+function saveEditor() {
+	configFileEditor.save();
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/libs/codemirror/codemirrorCustom.js
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/libs/codemirror/codemirrorCustom.js
@@ -1,40 +1,38 @@
-console.log("index.js invoked");
-
-console.log("start is active");
-
-var configFileTextArea;
-var type;
-var configFileEditor;
-var readOnly;
-var selected_row;
-//var nightMode;
+//console.log("index.js invoked");
+//
+//var configFileTextArea;
+//var type;
+//var configFileEditor;
+//var readOnly;
+//var selected_row;
+////var nightMode;
 //var theme;
-  	 
-/*	
-if (readOnly === undefined) {
-	readOnly = true;
-}
-*/ 	 
-//initTextArea();
-					 
-//var table = document.getElementById("file-form:file-table");
-//var rows = document.getElementsByTagName("tr");
-//	
-//for (let i = 0; i < rows.length; ++i) {
-//	var currentRow = table.rows[i];
-//	currentRow.setAttribute('id', 'row'+i);
-//	currentRow.onclick = function() {
-//	/*
-//		Array.from(this.parentElement.children).forEach(function(el){
-//			el.classList.remove('selected-row');
-//		});
-//	*/ 
-//	[...this.parentElement.children].forEach((el) => el.classList.remove("selected-row"));
-//	this.classList.add('selected-row');
-//	};
+//  	 
+///*	
+//if (readOnly === undefined) {
+//	readOnly = true;
 //}
-
-function initTextArea() {
+//*/ 	 
+////initTextArea();
+//					 
+////var table = document.getElementById("file-form:file-table");
+////var rows = document.getElementsByTagName("tr");
+////	
+////for (let i = 0; i < rows.length; ++i) {
+////	var currentRow = table.rows[i];
+////	currentRow.setAttribute('id', 'row'+i);
+////	currentRow.onclick = function() {
+////	/*
+////		Array.from(this.parentElement.children).forEach(function(el){
+////			el.classList.remove('selected-row');
+////		});
+////	*/ 
+////	[...this.parentElement.children].forEach((el) => el.classList.remove("selected-row"));
+////	this.classList.add('selected-row');
+////	};
+////}
+//
+//function initTextArea() {
 //	let fileTypeElement = document.getElementById("currentConfigFileType");
 //	console.log("fileTypeElement = " + fileTypeElement);
 //	if (fileTypeElement !== null){
@@ -43,19 +41,22 @@ function initTextArea() {
 //		type = "xml";
 //	}
 //	console.log("type = " + type);
-//	nightMode = document.getElementById("nightMode").innerHTML.trim(); // "true" or "false"
+////	nightMode = document.getElementById("nightMode").innerHTML.trim(); // "true" or "false"
 //	if (typeof type == "undefined") {
 //		type = "xml";
 //	}
-//	
-//	if (nightMode == "true") {
-//		theme = "blackboard";
-//	} else {
+//	if (typeof theme == "undefined") {
 //		theme = "default";
 //	}
+////	
+////	if (nightMode == "true") {
+////		theme = "blackboard";
+////	} else {
+////		theme = "default";
+////	}
+////
+//	configFileTextArea = document.getElementById("editor-form:editor");
 //
-//	configFileTextArea = document.getElementById("codemirrorFileContent");
-
 //	if (configFileTextArea !== null) {
 //		if (configFileEditor) {
 //			configFileEditor.toTextArea();
@@ -64,7 +65,8 @@ function initTextArea() {
 //		}
 //		configFileEditor = CodeMirror.fromTextArea(configFileTextArea, {
 //			lineNumbers: true,
-//			mode: 'xml',
+//			mode: type,
+//			theme: theme,
 //			readOnly: readOnly,
 //			autofocus: true,
 //			indextUnit: 4,
@@ -76,6 +78,9 @@ function initTextArea() {
 //					if (cm.getOption("fullScreen")) {
 //						cm.setOption("fullScreen", false);
 //					}
+//				},
+//				"Ctrl-D": function(cm) {
+//					cm.setOption("theme", cm.getOption("theme") == "default" ? "dracula" : "default");
 //				}
 //			}
 //			
@@ -88,35 +93,19 @@ function initTextArea() {
 //		}, 100);
 //*/ 
 //	}
- 
-}
-
-function setEditable(editable, number, isButton) {
-	selected_row = document.getElementById('row'+number);
-	
-	readOnly = !editable;
-	if (readOnly && isButton) {
-		alert("Die Rechte im Dateisystem müssen korrigiert werden um diese Datei zu bearbeiten.");
-	}
-	console.log('seteditable function on');
-}
-
-function saveEditor() {
-	configFileEditor.save();
-}
-
-
-
-function initCodemirrorEditor() {
-	let fileElement = document.getElementById('codemirrorFileContent')
-	let fileElementContent = fileElement.innerHTML;
-	
-	var editorContainerTest = document.getElementById('codeEditorContainer')
-	CodeMirror(editorContainerTest, {
-	  lineNumbers: false,
-	  value: fileElementContent,
-	  mode: 'application/xml',
-	})
-	console.log('init is working with xml');
-}
-
+// 
+//}
+//
+//function setEditable(editable, number, isButton) {
+//	selected_row = document.getElementById('row'+number);
+//	
+//	readOnly = !editable;
+//	if (readOnly && isButton) {
+//		alert("Die Rechte im Dateisystem müssen korrigiert werden um diese Datei zu bearbeiten.");
+//	}
+//}
+//
+//function saveEditor() {
+//	configFileEditor.save();
+//}
+//
