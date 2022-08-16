242af017c1dfa98b149cdfd769dbd5f6751e2205
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2022-08-16 17:07:33 +0200

    added refresh method for generating the files list; refs #22348

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/administration/configeditor/BackupRecord.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/administration/configeditor/BackupRecord.java
@@ -1,22 +1,46 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.goobi.viewer.model.administration.configeditor;
 
+import java.io.Serializable;
 
-public class BackupRecord {
-	
-	private String name;
-	private int number;
-	
-	public BackupRecord(String name, int i) {
-		this.name = name;
-		this.number = i;
-	}
+public class BackupRecord implements Serializable {
 
-	public String getName() {
-		return name;
-	}
+    private static final long serialVersionUID = -7935008140086500081L;
 
-	public int getNumber() {
-		return number;
-	}
-	
+    private String name;
+    private int number;
+
+    public BackupRecord(String name, int i) {
+        this.name = name;
+        this.number = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
 }
--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/administration/configeditor/FileRecord.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/administration/configeditor/FileRecord.java
@@ -1,56 +1,76 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.goobi.viewer.model.administration.configeditor;
 
+import java.io.Serializable;
 
-public class FileRecord {
+public class FileRecord implements Serializable {
 
-	private String fileName;
-	private int number;
-	private boolean readable;
-	private boolean writable;
-	private String fileType;
-	
-	public FileRecord() {
-		
-	}
-	
-	public FileRecord(String fileName, int number, boolean readable, boolean writable) {
-		this.fileName = fileName;
-		this.number = number;
-		this.readable = readable;
-		this.writable = writable;
-		this.fileType = getFileTypeFromName(fileName);
-	}
-	
-	private static String getFileTypeFromName(String name) {
-		int lastIndex = name.lastIndexOf(".");
-		if (lastIndex == -1) {
-			return "";
-		}
-		return name.substring(lastIndex+1);
-	}
+    private static final long serialVersionUID = -1074497836317565196L;
 
-	public String getFileName() {
-//		System.out.println("getFileName()");
-		return fileName;
-	}
+    private String fileName;
+    private int number;
+    private boolean readable;
+    private boolean writable;
+    private String fileType;
 
-	public int getNumber() {
-//		System.out.println("getNumber()");
-		return number;
-	}
+    public FileRecord() {
 
-	public boolean isReadable() {
-		return readable;
-	}
-	
-	public boolean isWritable() {
-		return writable;
-	}
+    }
 
-	public String getFileType() {
-		return fileType;
-	}
-	
-	
-	
+    public FileRecord(String fileName, int number, boolean readable, boolean writable) {
+        this.fileName = fileName;
+        this.number = number;
+        this.readable = readable;
+        this.writable = writable;
+        this.fileType = getFileTypeFromName(fileName);
+    }
+
+    private static String getFileTypeFromName(String name) {
+        int lastIndex = name.lastIndexOf(".");
+        if (lastIndex == -1) {
+            return "";
+        }
+        return name.substring(lastIndex + 1);
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isReadable() {
+        return readable;
+    }
+
+    public boolean isWritable() {
+        return writable;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
 }
