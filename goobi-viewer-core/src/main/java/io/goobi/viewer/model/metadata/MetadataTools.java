d652536885de5876c69ec792f3ddd7177275b9ca
Georg Mayr-Duffner <g.duffner@gmail.com>
2022-10-20 23:25:40 +0200

    Add meta tags for DOI
    This relies on a SOLR field `MD_PI_DOI_URL`. In many contexts, a DOI should be represented in form of a URL, prefixed with a resolver address like `https://doi.org/`. Therefore, such field should be present in every Viewer that employs DOIs.
    A machine readable representation is highly recommended for DOI landing pages (cf https://support.datacite.org/docs/landing-pages).

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataTools.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataTools.java
@@ -158,6 +158,10 @@ public class MetadataTools {
             identifier = structElement.getMetadataValue(SolrConstants.URN);
             result.append("\r\n<meta name=\"DC.identifier\" content=\"").append(identifier).append("\" scheme=\"DCTERMS.URI\" />");
         }
+        if (structElement.getMetadataValue("MD_PI_DOI_URL") != null) {
+            identifier = structElement.getMetadataValue("MD_PI_DOI_URL");
+            result.append("\r\n<meta name=\"DC.identifier\" content=\"").append(identifier).append("\" scheme=\"DCTERMS.URI\" />");
+        }
         // DCTERMS.abstract
         if (structElement.getMetadataValue("MD_INFORMATION") != null) {
             String value = structElement.getMetadataValue("MD_INFORMATION");
@@ -282,6 +286,12 @@ public class MetadataTools {
             String value = StringEscapeUtils.escapeHtml4(structElement.getMetadataValue("MD_INFORMATION"));
             result.append("\r\n<meta name=\"description\" content=\"").append(value).append("\" />");
         }
+        // citation_doi
+        if (structElement.getMetadataValue("MD_PI_DOI_URL") != null) {
+            String value = StringEscapeUtils.escapeHtml4(structElement.getMetadataValue("MD_PI_DOI_URL"));
+            result.append("\r\n<meta name=\"citation_doi\" content=\"").append(value).append("\" />");
+        }
+
 
         return result.toString();
     }
