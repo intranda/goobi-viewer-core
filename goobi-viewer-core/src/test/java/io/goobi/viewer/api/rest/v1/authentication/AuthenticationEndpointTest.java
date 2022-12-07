a00d81b0d4f8f3b1799349daf25b05c16d22ca93
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2022-12-07 13:15:41 +0100

    fixed test with queryParam; refs #20130

--- goobi-viewer-core/src/test/java/io/goobi/viewer/api/rest/v1/authentication/AuthenticationEndpointTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/api/rest/v1/authentication/AuthenticationEndpointTest.java
@@ -13,20 +13,20 @@ import io.goobi.viewer.model.security.authentication.HttpHeaderProvider;
 
 public class AuthenticationEndpointTest extends AbstractRestApiTest {
 
-//    /**
-//     * @see AuthenticationEndpoint#headerParameterLogin(String)
-//     * @verifies return status 403 if redirectUrl external
-//     */
-//    @Test
-//    public void headerParameterLogin_shouldReturnStatus403IfRedirectUrlExternal() throws Exception {
-//        String url = urls.path(ApiUrls.AUTH, ApiUrls.AUTH_HEADER, "?redirectUrl=https://example.com").build();
-//        try (Response response = target(url)
-//                .request()
-//                .get()) {
-//            assertEquals("Should return status 403", 403, response.getStatus());
-//            assertEquals(AuthenticationEndpoint.REASON_PHRASE_ILLEGAL_REDIRECT_URL, response.getStatusInfo().getReasonPhrase());
-//        }
-//    }
+    /**
+     * @see AuthenticationEndpoint#headerParameterLogin(String)
+     * @verifies return status 403 if redirectUrl external
+     */
+    @Test
+    public void headerParameterLogin_shouldReturnStatus403IfRedirectUrlExternal() throws Exception {
+        String url = urls.path(ApiUrls.AUTH, ApiUrls.AUTH_HEADER).build();
+        try (Response response = target(url).queryParam("redirectUrl", "https://example.com")
+                .request()
+                .get()) {
+            assertEquals("Should return status 403", 403, response.getStatus());
+            assertEquals(AuthenticationEndpoint.REASON_PHRASE_ILLEGAL_REDIRECT_URL, response.getStatusInfo().getReasonPhrase());
+        }
+    }
 
     /**
      * @see AuthenticationEndpoint#headerParameterLogin(String)
@@ -43,30 +43,30 @@ public class AuthenticationEndpointTest extends AbstractRestApiTest {
         }
     }
 
-//    /**
-//     * @see AuthenticationEndpoint#headerParameterLogin(String)
-//     * @verifies return status 403 if no matching provider found
-//     */
-//    @Test
-//    public void headerParameterLogin_shouldReturnStatus403IfNoMatchingProviderFound() throws Exception {
-//        String url = urls.path(ApiUrls.AUTH, ApiUrls.AUTH_HEADER).build();
-//        assertEquals(5, DataManager.getInstance()
-//                .getConfiguration()
-//                .getAuthenticationProviders()
-//                .size());
-//        DataManager.getInstance()
-//                .getConfiguration()
-//                .getAuthenticationProviders()
-//                .add(new HttpHeaderProvider("HTTP", "HTTP", "https://locahost:8080/viewer/index", null, 60000, "header", "someOtherParameter"));
-//        assertEquals(6, DataManager.getInstance()
-//                .getConfiguration()
-//                .getAuthenticationProviders()
-//                .size());
-//        try (Response response = target(url)
-//                .request()
-//                .get()) {
-//            assertEquals("Should return status 403", 403, response.getStatus());
-//            assertEquals(AuthenticationEndpoint.REASON_PHRASE_NO_PROVIDER_FOUND, response.getStatusInfo().getReasonPhrase());
-//        }
-//    }
+    //    /**
+    //     * @see AuthenticationEndpoint#headerParameterLogin(String)
+    //     * @verifies return status 403 if no matching provider found
+    //     */
+    //    @Test
+    //    public void headerParameterLogin_shouldReturnStatus403IfNoMatchingProviderFound() throws Exception {
+    //        String url = urls.path(ApiUrls.AUTH, ApiUrls.AUTH_HEADER).build();
+    //        assertEquals(5, DataManager.getInstance()
+    //                .getConfiguration()
+    //                .getAuthenticationProviders()
+    //                .size());
+    //        DataManager.getInstance()
+    //                .getConfiguration()
+    //                .getAuthenticationProviders()
+    //                .add(new HttpHeaderProvider("HTTP", "HTTP", "https://locahost:8080/viewer/index", null, 60000, "header", "someOtherParameter"));
+    //        assertEquals(6, DataManager.getInstance()
+    //                .getConfiguration()
+    //                .getAuthenticationProviders()
+    //                .size());
+    //        try (Response response = target(url)
+    //                .request()
+    //                .get()) {
+    //            assertEquals("Should return status 403", 403, response.getStatus());
+    //            assertEquals(AuthenticationEndpoint.REASON_PHRASE_NO_PROVIDER_FOUND, response.getStatusInfo().getReasonPhrase());
+    //        }
+    //    }
 }
