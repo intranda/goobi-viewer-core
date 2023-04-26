127630c799385f2e49eddd4a49c1d44d107f5d82
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-04-26 18:32:05 +0200

    GeoMapUpdate: added @SuppressWarnings

--- goobi-viewer-core/src/main/java/io/goobi/viewer/dao/update/GeoMapUpdate.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/dao/update/GeoMapUpdate.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.goobi.viewer.dao.update;
 
 import java.sql.SQLException;
@@ -23,42 +44,57 @@ public class GeoMapUpdate implements IModelUpdate {
     @Override
     public boolean update(IDAO dao, CMSTemplateManager templateManager) throws DAOException, SQLException {
         if (dao.columnsExists("cms_geomap", "solr_query")) {
-            
+
             Map<Long, FeatureSet> featureSets = createFeatureSets(dao);
-            
+
             List<GeoMap> maps = addToGeomap(featureSets, dao);
-            
+
             dao.executeUpdate("DELETE FROM cms_geomap_features");
-            
+
             for (GeoMap geoMap : maps) {
                 dao.updateGeoMap(geoMap);
             }
-            
+
             dao.executeUpdate("ALTER TABLE cms_geomap DROP COLUMN solr_query");
-            
+
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
+    /**
+     * 
+     * @param featureSets
+     * @param dao
+     * @return
+     * @throws DAOException
+     */
+    @SuppressWarnings("unchecked")
     List<GeoMap> addToGeomap(Map<Long, FeatureSet> featureSets, IDAO dao) throws DAOException {
         List<GeoMap> maps = new ArrayList<>();
-        for (Entry<Long, FeatureSet> entry  : featureSets.entrySet()) {
+        for (Entry<Long, FeatureSet> entry : featureSets.entrySet()) {
             Long geomapId = entry.getKey();
             FeatureSet featureSet = entry.getValue();
-            
+
             GeoMap geomap = dao.getGeoMap(geomapId);
             geomap.addFeatureSet(featureSet);
-            if(featureSet instanceof ManualFeatureSet) {
+            if (featureSet instanceof ManualFeatureSet) {
                 List<String> features = dao.getNativeQueryResults("SELECT features FROM cms_geomap_features WHERE geomap_id = " + geomapId + ";");
                 ((ManualFeatureSet) featureSet).setFeatures(features);
             }
             maps.add(geomap);
         }
+        
         return maps;
     }
 
+    /**
+     * 
+     * @param dao
+     * @return
+     * @throws DAOException
+     */
+    @SuppressWarnings("unchecked")
     Map<Long, FeatureSet> createFeatureSets(IDAO dao) throws DAOException {
         List<Object[]> info = dao.getNativeQueryResults("SHOW COLUMNS FROM cms_geomap");
 
@@ -68,33 +104,34 @@ public class GeoMapUpdate implements IModelUpdate {
 
         Map<Long, FeatureSet> featureSets = new HashMap<>();
         for (Object[] geomap : geomaps) {
-            
+
             Map<String, Object> columns = IntStream.range(0, columnNames.size())
                     .boxed()
                     .filter(i -> geomap[i] != null)
                     .collect(Collectors.toMap(i -> columnNames.get(i), i -> geomap[i]));
-                            
-                Long geomapId = Optional.ofNullable(columns.get("geomap_id")).map(o -> (Long) o).orElse(null);
-                Integer mapType = Optional.ofNullable(columns.get("map_type")).map(o -> (Integer) o).orElse(null);
-                String solrQuery = Optional.ofNullable(columns.get("solr_query")).map(o -> (String) o).orElse(null);
-                String markerTitleField = Optional.ofNullable(columns.get("marker_title_field")).map(o -> (String) o).orElse(null);
-                String marker = Optional.ofNullable(columns.get("marker")).map(o -> (String) o).orElse(null);
-                
-                if(mapType == null) {
-                    //skip
-                } else if(mapType == 1) {
-                    ManualFeatureSet featureSet = new ManualFeatureSet();
-                    featureSet.setMarker(marker);
-                    featureSets.put(geomapId, featureSet);
-                } else {
-                    SolrFeatureSet featureSet = new SolrFeatureSet();
-                    featureSet.setMarker(marker);
-                    featureSet.setSolrQuery(solrQuery);
-                    featureSet.setMarkerTitleField(markerTitleField);
-                    featureSets.put(geomapId, featureSet);
-                }
+
+            Long geomapId = Optional.ofNullable(columns.get("geomap_id")).map(o -> (Long) o).orElse(null);
+            Integer mapType = Optional.ofNullable(columns.get("map_type")).map(o -> (Integer) o).orElse(null);
+            String solrQuery = Optional.ofNullable(columns.get("solr_query")).map(o -> (String) o).orElse(null);
+            String markerTitleField = Optional.ofNullable(columns.get("marker_title_field")).map(o -> (String) o).orElse(null);
+            String marker = Optional.ofNullable(columns.get("marker")).map(o -> (String) o).orElse(null);
+
+            if (mapType == null) {
+                //skip
+            } else if (mapType == 1) {
+                ManualFeatureSet featureSet = new ManualFeatureSet();
+                featureSet.setMarker(marker);
+                featureSets.put(geomapId, featureSet);
+            } else {
+                SolrFeatureSet featureSet = new SolrFeatureSet();
+                featureSet.setMarker(marker);
+                featureSet.setSolrQuery(solrQuery);
+                featureSet.setMarkerTitleField(markerTitleField);
+                featureSets.put(geomapId, featureSet);
+            }
 
         }
+        
         return featureSets;
     }
 
