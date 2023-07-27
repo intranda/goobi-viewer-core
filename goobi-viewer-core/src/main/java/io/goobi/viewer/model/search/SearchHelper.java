75b11c30b92ba4e93eda80501ed96d12e792e3ae
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-07-27 12:30:33 +0200

    updated hyperlink regex to support multiple links per string; refs #25034

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/search/SearchHelper.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/search/SearchHelper.java
@@ -166,7 +166,7 @@ public final class SearchHelper {
     /** Constant <code>patternYearRange</code> */
     public static Pattern patternYearRange = Pattern.compile("\\[[0-9]+ TO [0-9]+\\]");
     /** Constant <code>patternHyperlink</code> */
-    public static Pattern patternHyperlink = Pattern.compile("(<a .*<\\/a>)");
+    public static Pattern patternHyperlink = Pattern.compile("(<a (?:(?!<\\/a>).)*<\\/a>)");
 
     public static final Pattern patternAllItems = Pattern.compile(
             "[+-]*\\((\\w+:\\\"[\\wäáàâöóòôüúùûëéèêßñ ]+\\\" *)+\\)|[+-]*\\(((\\w+:\\([\\wäáàâöóòôüúùûëéèêßñ ]+\\)) *)++\\)|[+-]*\\((\\w+:\\(\\[[\\wäáàâöóòôüúùûëéèêßñ]+ TO [\\wäáàâöóòôüúùûëéèêßñ]+\\]\\) *+)\\)");
@@ -1433,7 +1433,7 @@ public final class SearchHelper {
         if (term.length() < 2) {
             return phrase;
         }
-
+        
         StringBuilder sb = new StringBuilder();
         String normalizedPhrase = normalizeString(phrase);
         String normalizedTerm = normalizeString(term);
@@ -1478,7 +1478,7 @@ public final class SearchHelper {
             string = sb.toString();
         }
 
-        string = string.replaceAll(patternHyperlink.pattern(), " ");
+        // string = string.replaceAll(patternHyperlink.pattern(), " ");
         string = string.toLowerCase().replaceAll("\\p{M}", "").replaceAll("[^\\p{L}0-9#]", " ");
         string = Normalizer.normalize(string, Normalizer.Form.NFC);
         return string;
