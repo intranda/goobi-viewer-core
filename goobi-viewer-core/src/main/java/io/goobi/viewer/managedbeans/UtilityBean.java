c99dc83cce5a059af61f69331e4b44a4aad26634
Florian Alpers <florian.apers@intranda.com>
2023-08-02 15:32:26 +0200

    Handle empty date metadata

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/UtilityBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/UtilityBean.java
@@ -10,36 +10,46 @@ import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.StringUtils;
+
 import io.goobi.viewer.controller.DateTools;
 
 @Named("utils")
 @ApplicationScoped
-public class UtilityBean implements Serializable{
-    
+public class UtilityBean implements Serializable {
+
     public Map createMap(List list) {
         Map map = new HashMap<>();
-        if(list != null && list.size() > 1) {
-            for (int i = 0; i < list.size()-1; i+=2) {
+        if (list != null && list.size() > 1) {
+            for (int i = 0; i < list.size() - 1; i += 2) {
                 int keyIndex = i;
-                int valueIndex = i+1;
+                int valueIndex = i + 1;
                 Object key = list.get(keyIndex);
-                if(list.get(valueIndex) instanceof List) {
+                if (list.get(valueIndex) instanceof List) {
                     Map value = createMap((List) list.get(valueIndex));
-                    map.put(key,  value);
+                    map.put(key, value);
                 } else {
-                    map.put(key,  list.get(valueIndex));
+                    map.put(key, list.get(valueIndex));
                 }
             }
         }
         return map;
     }
-    
+
     public LocalDate getAsDate(String string) {
-        return DateTools.parseDateFromString(string).toLocalDate();
+        if (StringUtils.isNotBlank(string)) {
+            return DateTools.parseDateFromString(string).toLocalDate();
+        } else {
+            return null;
+        }
     }
-    
+
     public LocalDateTime getAsDateTime(String string) {
-        return DateTools.parseDateFromString(string);
+        if (StringUtils.isNotBlank(string)) {
+            return DateTools.parseDateFromString(string);
+        } else {
+            return null;
+        }
     }
 
 }
