34d6929ff6ab69e5bcc6fd66184b6475d515e828
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-09-07 16:44:58 +0200

    minor code smell; refs #20082

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/EventElement.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/EventElement.java
@@ -322,8 +322,7 @@ public class EventElement implements Comparable<EventElement>, Serializable {
     public List<Metadata> getMetadata() {
         ActiveDocumentBean adb = BeanUtils.getActiveDocumentBean();
         if (adb != null) {
-            List<Metadata> ret = Metadata.filterMetadata(metadata, adb.getSelectedRecordLanguage(), null);
-            return ret;
+            return Metadata.filterMetadata(metadata, adb.getSelectedRecordLanguage(), null);
         }
 
         return metadata;
