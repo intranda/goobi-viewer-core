2639ea222b795cdcc57d982f3ce20260d9cabaa8
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-10-12 16:14:23 +0200

    removed duplicate check; refs #20082

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/PhysicalElement.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/PhysicalElement.java
@@ -1445,8 +1445,7 @@ public class PhysicalElement implements Comparable<PhysicalElement>, Serializabl
      * @throws DAOException
      */
     public boolean isAccessPermissionImageDownload() throws IndexUnreachableException, DAOException {
-        if (FacesContext.getCurrentInstance() != null && FacesContext.getCurrentInstance() != null
-                && FacesContext.getCurrentInstance().getExternalContext() != null) {
+        if (FacesContext.getCurrentInstance() != null && FacesContext.getCurrentInstance().getExternalContext() != null) {
             HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
             return AccessConditionUtils.checkAccessPermissionByIdentifierAndFileNameWithSessionMap(request, pi, fileName,
                     IPrivilegeHolder.PRIV_DOWNLOAD_IMAGES).isGranted();
