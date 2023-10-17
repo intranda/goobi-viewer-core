ae3e58b65c0477ff378d838e90d227bc627b819e
Florian Alpers <florian.apers@intranda.com>
2023-10-17 09:49:17 +0200

    Fixed cron expression of geomap update task

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/job/TaskType.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/job/TaskType.java
@@ -42,7 +42,7 @@ public enum TaskType {
     /** Write single page pdfs to storage to be used when creating a full record pdf **/
     PRERENDER_PDF("0 35 0 * * ?"),
     /** Fill all CMS-Geomaps with features from SOLR to avoid loading that data during page load */
-    CACHE_GEOMAPS("*/5 * * * *");
+    CACHE_GEOMAPS("0 /5 * * * ?");
 
     private final String defaultCronExpression;
 
