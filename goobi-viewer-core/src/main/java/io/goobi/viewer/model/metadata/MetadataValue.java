27867b8e972bb53b33d15303c351e925912db802
Florian Alpers <florian.apers@intranda.com>
2023-10-18 16:20:55 +0200

    revert c:forEachs in metadata list to ui:repeat and build metadata value in java rather than xhtml

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataValue.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataValue.java
@@ -27,12 +27,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import de.undercouch.citeproc.CSL;
 import io.goobi.viewer.managedbeans.utils.BeanUtils;
@@ -455,6 +458,11 @@ public class MetadataValue implements Serializable {
         this.ownerIddoc = ownerIddoc;
     }
 
+    public String getDisplayValue(Locale locale) {
+        String[] comboValues = IntStream.range(0, paramValues.size()).mapToObj(this::getComboValueShort).toArray(String[]::new);
+        return ViewerResourceBundle.getTranslationWithParameters(getMasterValue(), locale, comboValues);
+    }
+    
     /**
      * <p>
      * Getter for the field <code>masterValue</code>.
