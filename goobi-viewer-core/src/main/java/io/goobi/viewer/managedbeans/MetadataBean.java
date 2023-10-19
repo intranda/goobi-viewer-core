b47419241c4e984f0a3dbc236248bbd4980dfd4f
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-10-19 15:06:25 +0200

    clear metadata element list every time metadata is loaded; fixes #25327

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/MetadataBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/MetadataBean.java
@@ -50,6 +50,7 @@ import io.goobi.viewer.model.metadata.MetadataElement;
 import io.goobi.viewer.model.metadata.MetadataView;
 import io.goobi.viewer.model.viewer.EventElement;
 import io.goobi.viewer.model.viewer.StructElement;
+import jnr.ffi.Struct.key_t;
 
 /**
  * Provides the metadata for the current structure and event elements.
@@ -112,12 +113,8 @@ public class MetadataBean {
         }
 
         logger.trace("loadMetadata for: {}", currentElement.getLabel());
-        List<MetadataElement> metadataElementList = metadataElementMap.get(index);
-        if (metadataElementList == null) {
-            metadataElementList = new ArrayList<>();
-            metadataElementMap.put(index, metadataElementList);
-        }
-
+        List<MetadataElement> metadataElementList = metadataElementMap.computeIfAbsent(index, k -> new ArrayList<>());
+        metadataElementList.clear(); // Clear hierarchy to avoid duplicates when switching language
         try {
             metadataElementList.add(new MetadataElement().init(currentElement, index, locale)
                     .setSelectedRecordLanguage(activeDocumentBean.getSelectedRecordLanguage()));
@@ -163,8 +160,9 @@ public class MetadataBean {
      * @return the metadataElementList
      */
     public List<MetadataElement> getMetadataElementList(int index) {
-        //        logger.trace("getMetadataElementList({})", index);
+        // logger.trace("getMetadataElementList({})", index);
         Locale locale = BeanUtils.getLocale();
+
         if (metadataElementMap.get(index) == null || !Objects.equals(locale, this.currentMetadataLocale)) {
             // Only reload if empty, otherwise a c:forEach (used by p:tabView) will cause a reload on every iteration
             try {
