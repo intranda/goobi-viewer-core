f619ff6613fb6b9be70679e3b575ef52f0b992c8
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2023-12-07 16:33:57 +0100

    HightLightTest extends AbstractSolrEnabledTest

--- goobi-viewer-core/src/test/java/io/goobi/viewer/model/cms/HighlightTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/cms/HighlightTest.java
@@ -31,6 +31,7 @@ import java.time.LocalDateTime;
 
 import org.junit.Test;
 
+import io.goobi.viewer.AbstractSolrEnabledTest;
 import io.goobi.viewer.controller.Configuration;
 import io.goobi.viewer.controller.DataManager;
 import io.goobi.viewer.controller.imaging.ThumbnailHandler;
@@ -40,7 +41,7 @@ import io.goobi.viewer.exceptions.ViewerConfigurationException;
 import io.goobi.viewer.model.cms.HighlightData.ImageMode;
 import io.goobi.viewer.model.cms.media.CMSMediaItem;
 
-public class HighlightTest {
+public class HighlightTest extends AbstractSolrEnabledTest {
 
     Configuration config = DataManager.getInstance().getConfiguration();
 
