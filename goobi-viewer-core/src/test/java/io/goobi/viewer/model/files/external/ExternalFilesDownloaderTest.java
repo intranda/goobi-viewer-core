0bd385c3dde21aadf1e620fb58a724eb05136243
Florian Alpers <florian.apers@intranda.com>
2024-02-01 08:59:27 +0100

    updated tests to junit5

--- goobi-viewer-core/src/test/java/io/goobi/viewer/model/files/external/ExternalFilesDownloaderTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/files/external/ExternalFilesDownloaderTest.java
@@ -6,16 +6,13 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockserver.model.Header;
 import org.mockserver.model.HttpRequest;
@@ -23,14 +20,14 @@ import org.mockserver.model.HttpResponse;
 
 import io.goobi.viewer.TestServlet;
 
-public class ExternalFilesDownloaderTest {
+class ExternalFilesDownloaderTest {
 
     private final Path testZipFile = Path.of("src/test/resources/data/viewer/external-files/1287088031.zip");
     private final Path downloadFolder = Path.of("src/test/resources/output/external-files");
     private final TestServlet server = new TestServlet("127.0.0.1", 9191);
 
     @Test
-    public void test() throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    void test() throws IOException, InterruptedException, ExecutionException, TimeoutException {
 
         byte[] body = Files.readAllBytes(testZipFile);
         server.getServerClient()
@@ -38,9 +35,6 @@ public class ExternalFilesDownloaderTest {
                 .respond(HttpResponse.response().withHeader(new Header("Content-Type", "application/zip")).withBody(body));
 
         assertTrue(Files.isDirectory(downloadFolder) || Files.createDirectory(downloadFolder) != null);
-        //        URI uri = testZipFile.toAbsolutePath().toUri();
-        //        URI uri = URI.create("https://d-nb.info/1287088031/34");
-//                URI uri = URI.create("https://www.splittermond.de/wp-content/uploads/2016/01/Splittermond_GRW_erratiert.pdf");
             Consumer consumer = Mockito.spy(Consumer.class);
             URI uri = URI.create("http://127.0.0.1:9191/exteral/files/1287088031.zip");
             ExternalFilesDownloader download = new ExternalFilesDownloader(downloadFolder, consumer);
@@ -49,8 +43,8 @@ public class ExternalFilesDownloaderTest {
             assertTrue(Files.exists(downloadPath));
     }
 
-    @After
-    public void after() throws Exception {
+    @AfterEach
+    void after() throws Exception {
         server.shutdown();
         if (Files.exists(downloadFolder)) {
             FileUtils.deleteDirectory(downloadFolder.toFile());
--- goobi-viewer-core/src/test/java/io/goobi/viewer/model/files/external/ExternalResourceTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/files/external/ExternalResourceTest.java
@@ -2,24 +2,23 @@ package io.goobi.viewer.model.files.external;
 
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mockitoSession;
 
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
 import java.util.function.Consumer;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 
-public class ExternalResourceTest {
+class ExternalResourceTest {
 
     private final URI externalResourceUri = URI.create("https://d-nb.info/1287088031/34");
     private final Path downloadFolder = Path.of("src/test/resources/output/external-files");
 
     @Test
-    public void testCheckExistance() {
+    void testCheckExistance() {
         Consumer consumer = Mockito.spy(Consumer.class);
         ExternalResource resource = new ExternalResource(externalResourceUri, new ExternalFilesDownloader(downloadFolder, consumer));
         Mockito.verify(consumer, Mockito.never()).accept(Mockito.any(Progress.class));
@@ -27,7 +26,7 @@ public class ExternalResourceTest {
     }
     
     @Test
-    public void testDownload() throws IOException {
+    void testDownload() throws IOException {
         Consumer consumer = Mockito.spy(Consumer.class);
         ExternalResource resource = new ExternalResource(externalResourceUri, new ExternalFilesDownloader(downloadFolder, consumer));
         resource.downloadResource();
