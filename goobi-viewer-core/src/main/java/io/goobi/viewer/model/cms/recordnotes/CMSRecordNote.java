561d22f2fcb0841a9287e2c9c4daccb4c533bff0
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-02-23 21:12:00 +0100

    checkstyle: model/cms/recordnotes; refs #25581

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/cms/recordnotes/CMSRecordNote.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/cms/recordnotes/CMSRecordNote.java
@@ -93,7 +93,7 @@ public abstract class CMSRecordNote implements Serializable {
     }
 
     /**
-     * @param o
+     * @param source
      */
     protected CMSRecordNote(CMSRecordNote source) {
         this.id = source.id;
@@ -118,18 +118,23 @@ public abstract class CMSRecordNote implements Serializable {
      */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         CMSRecordNote other = (CMSRecordNote) obj;
         if (id == null) {
-            if (other.id != null)
+            if (other.id != null) {
                 return false;
-        } else if (!id.equals(other.id))
+            }
+        } else if (!id.equals(other.id)) {
             return false;
+        }
         return true;
     }
 
