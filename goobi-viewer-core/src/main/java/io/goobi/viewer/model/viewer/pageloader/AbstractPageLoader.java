92fe5439e55ec037537342c636fc28166b91876c
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-03-19 10:25:01 +0100

    added comment; refs #25438

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/pageloader/AbstractPageLoader.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/viewer/pageloader/AbstractPageLoader.java
@@ -100,6 +100,7 @@ public abstract class AbstractPageLoader implements IPageLoader {
     public static AbstractPageLoader create(StructElement topStructElement, boolean loadPages)
             throws IndexUnreachableException, PresentationException, DAOException {
         if (!loadPages) {
+            // Page loader that skips loading any pages for speed (e.g. TOC creation via REST)
             return new EmptyPageLoader(topStructElement);
         }
         
@@ -110,7 +111,7 @@ public abstract class AbstractPageLoader implements IPageLoader {
      * 
      * @param topStructElement
      * @param pageNosToLoad List of page numbers to load; empty list means all pages
-     * @return
+     * @return Appropriate page loader implementation for the given record topStructElement
      * @throws IndexUnreachableException
      * @throws PresentationException
      * @throws DAOException
