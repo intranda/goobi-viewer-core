02ced9943092a1d686d1f9aa7aee3ef77450dfd6
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-03-25 18:12:15 +0100

    Revert "VariableReplacedTest implements AbstractTest; refs #25871"
    This reverts commit 9c6424a6a1ef5819b39bf150c2a09d44bd833db7.

--- goobi-viewer-core/src/test/java/io/goobi/viewer/controller/variablereplacer/VariableReplacerTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/controller/variablereplacer/VariableReplacerTest.java
@@ -1,34 +1,12 @@
-/*
- * This file is part of the Goobi viewer - a content presentation and management
- * application for digitized objects.
- *
- * Visit these websites for more information.
- *          - http://www.intranda.com
- *          - http://digiverso.com
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package io.goobi.viewer.controller.variablereplacer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import io.goobi.viewer.AbstractTest;
 import io.goobi.viewer.controller.Configuration;
 
-class VariableReplacerTest extends AbstractTest {
+class VariableReplacerTest {
 
     private static final String PULL_THEME = "{config-folder-path}/script_theme-pull.sh {theme-path}";
     private static final String CREATE_DEVELOPER_PACKAGE = "{config-folder-path}/script_create_package.sh viewer {base-path} /var/www {solr-url}";
