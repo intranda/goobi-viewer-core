d7704d2436ba1ab62a5d509365ae45a436ae8da4
Florian Alpers <florian.apers@intranda.com>
2024-04-17 18:21:50 +0200

    use iiif manifest urls from solr field if so configured

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/Configuration.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/Configuration.java
@@ -4982,6 +4982,14 @@ public class Configuration extends AbstractConfiguration {
         return getLocalList("webapi.iiif.license", new ArrayList<>());
     }
 
+    public boolean useExternalManifestUrls() {
+        return getLocalBoolean("webapi.iiif.externalManifests[@enabled]", false);
+    }
+
+    public String getExternalManifestSolrField() {
+        return getLocalString("webapi.iiif.externalManifests.solrField", "");
+    }
+
     /**
      * <p>
      * getIIIFMetadataFields.
