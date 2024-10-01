72daab2bb35ee3f2d332aa58348a789b6136e562
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-10-01 15:24:16 +0200

    removed obsolete imports

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/ConfigurationBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/ConfigurationBean.java
@@ -22,8 +22,6 @@
 package io.goobi.viewer.managedbeans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -52,8 +50,6 @@ import io.goobi.viewer.exceptions.PresentationException;
 import io.goobi.viewer.exceptions.ViewerConfigurationException;
 import io.goobi.viewer.managedbeans.utils.BeanUtils;
 import io.goobi.viewer.messages.ViewerResourceBundle;
-import io.goobi.viewer.model.citation.CitationLink;
-import io.goobi.viewer.model.citation.CitationLink.CitationLinkLevel;
 import io.goobi.viewer.model.job.download.DownloadOption;
 import io.goobi.viewer.model.maps.GeoMapMarker;
 import io.goobi.viewer.model.metadata.Metadata;
