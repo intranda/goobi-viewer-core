f03d2873cf0f78b169c41367273cf424b7f52691
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-10-11 15:50:48 +0200

    do not handle "other" mime type for now

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/imaging/ThumbnailHandler.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/imaging/ThumbnailHandler.java
@@ -886,8 +886,8 @@ public class ThumbnailHandler {
                 case "object":
                     ret = getThumbnailPath(OBJECT_3D_THUMB).toString();
                     break;
-                case "other":
-                    break;
+//                case "other":
+//                    break;
                 default:
                     if (logger.isWarnEnabled()) {
                         logger.warn("Mime type of '{}' not supported: {}", doc.getLuceneId(), baseMimeType);
