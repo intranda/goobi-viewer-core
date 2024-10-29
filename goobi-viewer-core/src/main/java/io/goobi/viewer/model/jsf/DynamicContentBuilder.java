88f4f6bbb688cdbe811dda25cc193f21c350335d
Jan Vonde <jan.vonde@intranda.com>
2024-10-29 10:38:18 +0100

    more work in progress

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/jsf/DynamicContentBuilder.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/jsf/DynamicContentBuilder.java
@@ -27,7 +27,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 
-import javax.el.ELException;
+import jakarta.el.ELException;
 import jakarta.faces.FacesException;
 import jakarta.faces.application.Application;
 import jakarta.faces.application.Resource;
