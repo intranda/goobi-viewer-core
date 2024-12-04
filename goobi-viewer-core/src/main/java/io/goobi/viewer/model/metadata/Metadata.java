447a25d1d4e8dbbdb612fbef9a377e792d7125b8
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-12-04 15:27:11 +0100

    hide "metadata_restricted_access" values; refs #26011

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/Metadata.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/Metadata.java
@@ -919,6 +919,13 @@ public class Metadata implements Serializable {
                         }
                         found = true;
                         String value = val;
+
+                        // Set value to empty for restricted access metadata values when configured as non-grouped
+                        if (StringConstants.ACCESSCONDITION_METADATA_ACCESS_RESTRICTED.equals(value)) {
+                            logger.trace("Removing hidden value");
+                            value = "";
+                        }
+
                         // Apply replace rules
                         if (!param.getReplaceRules().isEmpty()) {
                             value = MetadataTools.applyReplaceRules(value, param.getReplaceRules(), se.getPi());
