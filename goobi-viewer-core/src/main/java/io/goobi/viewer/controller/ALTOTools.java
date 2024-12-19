8400f159f4fd157c23c975d5b34ff154d8024bf6
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2024-12-19 19:57:48 +0100

    added more problematic characters to filter out; refs #26623

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/ALTOTools.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/ALTOTools.java
@@ -92,7 +92,7 @@ public final class ALTOTools {
     public static final String TAG_LABEL_IGNORE_REGEX =
             "(^[^a-zA-ZÄäÁáÀàÂâÖöÓóÒòÔôÜüÚúÙùÛûëÉéÈèÊêßñ]+)|([^a-zA-ZÄäÁáÀàÂâÖöÓóÒòÔôÜüÚúÙùÛûëÉéÈèÊêßñ]+$)";
     /** Characters that can cause an "Invalid UTF-8 middle byte" error in the parser. */
-    public static final String ALTO_PROBLEMATIC_CHARS = "[ﬅ]";
+    public static final String ALTO_PROBLEMATIC_CHARS = "[ﬅ|ﬆ|ﬃ|ﬄ|ﬂ|ﬁ|�]";
 
     /**
      * Private constructor.
