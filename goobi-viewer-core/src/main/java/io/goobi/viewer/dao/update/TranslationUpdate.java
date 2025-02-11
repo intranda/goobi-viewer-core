84da9c87e8bdb27c56724b79fcefb0c3507f89b6
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-02-11 10:58:20 +0100

    added TranslationUpdate; refs #26134

---
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/dao/update/TranslationUpdate.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.goobi.viewer.dao.update;
+
+import java.sql.SQLException;
+
+import io.goobi.viewer.dao.IDAO;
+import io.goobi.viewer.exceptions.DAOException;
+import io.goobi.viewer.model.cms.pages.CMSTemplateManager;
+
+public class TranslationUpdate implements IModelUpdate {
+
+    private static final String[] TABLES = { "cs_campaign_translations", "cms_geomap_translation", "terms_of_use_translations", "translations" };
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean update(IDAO dao, CMSTemplateManager templateManager) throws DAOException, SQLException {
+        // Update column names
+        for (String table : TABLES) {
+            if (dao.tableExists(table)) {
+                dao.executeUpdate("ALTER TABLE '" + table + "' RENAME COLUMN 'value' TO 'translation_value'");
+            }
+        }
+
+        return true;
+    }
+}
