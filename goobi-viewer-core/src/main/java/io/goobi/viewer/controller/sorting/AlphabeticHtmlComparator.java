54071f8e9b5915acadf15b3f9bb756f5d15d4ab9
Florian Alpers <florian.apers@intranda.com>
2025-02-14 10:31:11 +0100

    add licence headers

--- goobi-viewer-core/src/main/java/io/goobi/viewer/controller/sorting/AlphabeticHtmlComparator.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/controller/sorting/AlphabeticHtmlComparator.java
@@ -1,3 +1,28 @@
+/*
+
+ * The Alphanum Algorithm is an improved sorting algorithm for strings
+ * containing numbers.  Instead of sorting numbers in ASCII order like
+ * a standard sort, this algorithm sorts numbers in numeric order.
+ *
+ * The Alphanum Algorithm is discussed at http://www.DaveKoelle.com
+ *
+ * Last modified on 2013-02-01 by intranda GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package io.goobi.viewer.controller.sorting;
 
 import java.text.Collator;
