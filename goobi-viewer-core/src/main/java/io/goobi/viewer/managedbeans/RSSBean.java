78430e2f68a6d16e3f54aa91a6f507efdac49aea
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-02-25 15:53:42 +0100

    use XHTML for the CMS RSS component (+ legacy) instead of JS; refs #26499

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/RSSBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/RSSBean.java
@@ -24,17 +24,25 @@ package io.goobi.viewer.managedbeans;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.rometools.rome.feed.synd.SyndEntry;
 
+import de.unigoettingen.sub.commons.contentlib.exceptions.ContentLibException;
+import io.goobi.viewer.controller.DataManager;
 import io.goobi.viewer.exceptions.DAOException;
 import io.goobi.viewer.exceptions.IndexUnreachableException;
 import io.goobi.viewer.exceptions.PresentationException;
 import io.goobi.viewer.exceptions.ViewerConfigurationException;
 import io.goobi.viewer.managedbeans.utils.BeanUtils;
+import io.goobi.viewer.model.rss.Channel;
 import io.goobi.viewer.model.rss.RSSFeed;
+import io.goobi.viewer.model.search.SearchAggregationType;
+import io.goobi.viewer.model.search.SearchFacets;
+import io.goobi.viewer.model.search.SearchHelper;
+import io.goobi.viewer.servlets.utils.ServletUtils;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
@@ -62,5 +70,25 @@ public class RSSBean {
         }
 
         return Collections.emptyList();
+    }
+
+    /**
+     * 
+     * @param maxHits
+     * @param query
+     * @param sortField
+     * @param sortDescending
+     * @return {@link Channel}
+     */
+    public Channel getRssFeedChannel(Integer maxHits, String query, String sortField, Boolean sortDescending) {
+        try {
+            return RSSFeed.createRssResponse(null, maxHits, null, query, null, BeanUtils.getRequest(), sortField,
+                    sortDescending == null || sortDescending);
+
+        } catch (ContentLibException e) {
+            logger.error(e.getMessage());
+        }
+
+        return null;
     }
 }
