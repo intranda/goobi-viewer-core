142c56b61df49ee8fdfd3b6a2e701552ead7d346
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-03-03 15:54:57 +0100

    log sibling query on TRACE: refs #26779

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/toc/TocMaker.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/toc/TocMaker.java
@@ -711,7 +711,7 @@ public final class TocMaker {
                         .append(SolrConstants.PI)
                         .append(":*")
                         .toString();
-                logger.debug("Sibling query: {}", siblingQuery);
+                logger.trace("Sibling query: {}", siblingQuery);
                 SolrDocumentList childDocs = DataManager.getInstance()
                         .getSearchIndex()
                         .search(siblingQuery, SolrSearchIndex.MAX_HITS,
