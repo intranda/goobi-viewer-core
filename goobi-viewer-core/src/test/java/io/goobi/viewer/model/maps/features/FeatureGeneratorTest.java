d785e47dd3f6407cb7d81228db15ad4bb83219fd
Florian Alpers <florian.apers@intranda.com>
2025-04-11 14:56:47 +0200

    test class

---
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/maps/features/FeatureGeneratorTest.java
@@ -0,0 +1,14 @@
+package io.goobi.viewer.model.maps.features;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FeatureGeneratorTest {
+
+    @Test
+    void test_findFeatureInDocument() {
+        FeatureGenerator generator = new FEa
+    }
+
+}
