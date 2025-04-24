0fd35ad0dc05df9c91d8ca35cecc5339587d015d
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-04-24 14:30:55 +0200

    Revert "commented out different tests for testing purposes"
    This reverts commit 70aeffac08c242c4d00b9ca6e478bd472ae0f78d.

--- goobi-viewer-core/src/test/java/io/goobi/viewer/controller/IndexerToolsTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/controller/IndexerToolsTest.java
@@ -34,93 +34,93 @@ import io.goobi.viewer.AbstractTest;
 
 class IndexerToolsTest extends AbstractTest {
 
-//    /**
-//     * @see IndexerTools#deleteRecord(String,boolean,Path)
-//     * @verifies create delete file correctly
-//     */
-//    @Test
-//    void deleteRecord_shouldCreateDeleteFileCorrectly() throws Exception {
-//        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
-//        if (!Files.isDirectory(hotfolder)) {
-//            Files.createDirectory(hotfolder);
-//        }
-//        Path file = Paths.get(hotfolder.toAbsolutePath().toString(), "PPN123.delete");
-//        try {
-//            Assertions.assertTrue(IndexerTools.deleteRecord("PPN123", true, hotfolder));
-//            Assertions.assertTrue(Files.isRegularFile(file));
-//        } finally {
-//            if (Files.isRegularFile(file)) {
-//                Files.delete(file);
-//            }
-//            if (!Files.isDirectory(hotfolder)) {
-//                Files.delete(hotfolder);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * @see IndexerTools#deleteRecord(String,boolean,Path)
-//     * @verifies create purge file correctly
-//     */
-//    @Test
-//    void deleteRecord_shouldCreatePurgeFileCorrectly() throws Exception {
-//        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
-//        if (!Files.isDirectory(hotfolder)) {
-//            Files.createDirectory(hotfolder);
-//        }
-//        Path file = Paths.get(hotfolder.toAbsolutePath().toString(), "PPN123.purge");
-//        try {
-//            Assertions.assertTrue(IndexerTools.deleteRecord("PPN123", false, hotfolder));
-//            Assertions.assertTrue(Files.isRegularFile(file));
-//        } finally {
-//            if (Files.isRegularFile(file)) {
-//                Files.delete(file);
-//            }
-//            if (!Files.isDirectory(hotfolder)) {
-//                Files.delete(hotfolder);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * @see IndexerTools#findNamingScheme(String,String,File[])
-//     * @verifies return original baseName if no files exist
-//     */
-//    @Test
-//    void findNamingScheme_shouldReturnOriginalBaseNameIfNoFilesExist() throws Exception {
-//        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
-//        try {
-//            if (!Files.exists(hotfolder)) {
-//                Files.createDirectory(hotfolder);
-//            }
-//            Assertions.assertTrue(Files.isDirectory(hotfolder));
-//
-//            Assertions.assertEquals("foo", IndexerTools.findNamingScheme("foo", "xml"));
-//        } finally {
-//            FileUtils.deleteDirectory(hotfolder.toFile());
-//        }
-//    }
-//
-//    /**
-//     * @see IndexerTools#findNamingScheme(String,String,File[])
-//     * @verifies return alternative naming scheme if initial name already exists
-//     */
-//    @Test
-//    void findNamingScheme_shouldReturnAlternativeNamingSchemeIfInitialNameAlreadyExists() throws Exception {
-//        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
-//        try {
-//            if (!Files.exists(hotfolder)) {
-//                Files.createDirectory(hotfolder);
-//            }
-//            Assertions.assertTrue(Files.isDirectory(hotfolder));
-//
-//            Path dataFolder = Paths.get(hotfolder.toAbsolutePath().toString(), "foo_data");
-//            Files.createDirectory(dataFolder);
-//            Assertions.assertTrue(Files.isDirectory(dataFolder));
-//
-//            Assertions.assertTrue(IndexerTools.findNamingScheme("foo", "xml", dataFolder.toFile()).startsWith("foo#"));
-//        } finally {
-//            FileUtils.deleteDirectory(hotfolder.toFile());
-//        }
-//    }
+    /**
+     * @see IndexerTools#deleteRecord(String,boolean,Path)
+     * @verifies create delete file correctly
+     */
+    @Test
+    void deleteRecord_shouldCreateDeleteFileCorrectly() throws Exception {
+        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
+        if (!Files.isDirectory(hotfolder)) {
+            Files.createDirectory(hotfolder);
+        }
+        Path file = Paths.get(hotfolder.toAbsolutePath().toString(), "PPN123.delete");
+        try {
+            Assertions.assertTrue(IndexerTools.deleteRecord("PPN123", true, hotfolder));
+            Assertions.assertTrue(Files.isRegularFile(file));
+        } finally {
+            if (Files.isRegularFile(file)) {
+                Files.delete(file);
+            }
+            if (!Files.isDirectory(hotfolder)) {
+                Files.delete(hotfolder);
+            }
+        }
+    }
+
+    /**
+     * @see IndexerTools#deleteRecord(String,boolean,Path)
+     * @verifies create purge file correctly
+     */
+    @Test
+    void deleteRecord_shouldCreatePurgeFileCorrectly() throws Exception {
+        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
+        if (!Files.isDirectory(hotfolder)) {
+            Files.createDirectory(hotfolder);
+        }
+        Path file = Paths.get(hotfolder.toAbsolutePath().toString(), "PPN123.purge");
+        try {
+            Assertions.assertTrue(IndexerTools.deleteRecord("PPN123", false, hotfolder));
+            Assertions.assertTrue(Files.isRegularFile(file));
+        } finally {
+            if (Files.isRegularFile(file)) {
+                Files.delete(file);
+            }
+            if (!Files.isDirectory(hotfolder)) {
+                Files.delete(hotfolder);
+            }
+        }
+    }
+
+    /**
+     * @see IndexerTools#findNamingScheme(String,String,File[])
+     * @verifies return original baseName if no files exist
+     */
+    @Test
+    void findNamingScheme_shouldReturnOriginalBaseNameIfNoFilesExist() throws Exception {
+        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
+        try {
+            if (!Files.exists(hotfolder)) {
+                Files.createDirectory(hotfolder);
+            }
+            Assertions.assertTrue(Files.isDirectory(hotfolder));
+
+            Assertions.assertEquals("foo", IndexerTools.findNamingScheme("foo", "xml"));
+        } finally {
+            FileUtils.deleteDirectory(hotfolder.toFile());
+        }
+    }
+
+    /**
+     * @see IndexerTools#findNamingScheme(String,String,File[])
+     * @verifies return alternative naming scheme if initial name already exists
+     */
+    @Test
+    void findNamingScheme_shouldReturnAlternativeNamingSchemeIfInitialNameAlreadyExists() throws Exception {
+        Path hotfolder = Paths.get(DataManager.getInstance().getConfiguration().getHotfolder());
+        try {
+            if (!Files.exists(hotfolder)) {
+                Files.createDirectory(hotfolder);
+            }
+            Assertions.assertTrue(Files.isDirectory(hotfolder));
+
+            Path dataFolder = Paths.get(hotfolder.toAbsolutePath().toString(), "foo_data");
+            Files.createDirectory(dataFolder);
+            Assertions.assertTrue(Files.isDirectory(dataFolder));
+
+            Assertions.assertTrue(IndexerTools.findNamingScheme("foo", "xml", dataFolder.toFile()).startsWith("foo#"));
+        } finally {
+            FileUtils.deleteDirectory(hotfolder.toFile());
+        }
+    }
 }
