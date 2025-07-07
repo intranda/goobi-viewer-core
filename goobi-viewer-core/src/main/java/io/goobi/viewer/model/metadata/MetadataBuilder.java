6b4cf735baa17662f9d16fd8f63d31a318ed0eb8
Florian Alpers <florian.apers@intranda.com>
2025-07-07 10:15:05 +0200

    update commons-collections dependency

--- goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataBuilder.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/model/metadata/MetadataBuilder.java
@@ -36,7 +36,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
