733e76b267a147d1438648a5789f3ba6feb2f2a1
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-10-06 12:49:47 +0200

    added MaintenanceModeTest; refs #20082

---
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/administration/MaintenanceModeTest.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Goobi viewer - a content presentation and management
+ * application for digitized objects.
+ *
+ * Visit these websites for more information.
+ *          - http://www.intranda.com
+ *          - http://digiverso.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.goobi.viewer.model.administration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.goobi.viewer.AbstractDatabaseEnabledTest;
+
+class MaintenanceModeTest extends AbstractDatabaseEnabledTest {
+
+    /**
+     * @see MaintenanceMode#getTextOrDefault(String)
+     * @verifies return translation correctly
+     */
+    @Test
+    void getTextOrDefault_shouldReturnTranslationCorrectly() throws Exception {
+        MaintenanceMode mm = new MaintenanceMode();
+        mm.setText("foo", "en");
+        assertEquals("foo", mm.getTextOrDefault("en"));
+        assertEquals("foo", mm.getTextOrDefault("de"));
+    }
+
+}
