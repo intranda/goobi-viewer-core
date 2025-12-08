d0ce85d442c01d82f730ee452ff940403786d73e
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-12-08 16:38:08 +0100

    Revert "AccessConditionUtils.generateAccessCheckQuery(): do not query for FILENAME_PLAIN, as it's no longer written"
    This reverts commit 24fea3030cc78f5d828958278a9acf669a8e9f35.

--- goobi-viewer-core/src/test/java/io/goobi/viewer/model/security/AccessConditionUtilsTest.java
+++ goobi-viewer-core/src/test/java/io/goobi/viewer/model/security/AccessConditionUtilsTest.java
@@ -254,7 +254,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies use correct field name for AV files
      */
     @Test
-    void generateAccessCheckQuery_shouldUseCorrectFieldNameForAVFiles() {
+    void generateAccessCheckQuery_shouldUseCorrectFieldNameForAVFiles() throws Exception {
         {
             String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001.tif");
             Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +(" + SolrConstants.FILENAME + ":\"00000001.tif\" "
@@ -283,11 +283,12 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies use correct file name for text files
      */
     @Test
-    void generateAccessCheckQuery_shouldUseCorrectFileNameForTextFiles() {
+    void generateAccessCheckQuery_shouldUseCorrectFileNameForTextFiles() throws Exception {
         {
             String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "alto/PPN123456789/00000001.txt");
             Assertions.assertEquals(
-                    "+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME_FULLTEXT + ":\"alto/PPN123456789/00000001.txt\"",
+                    "+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +(" + SolrConstants.FILENAME_FULLTEXT
+                            + ":\"alto/PPN123456789/00000001.txt\" FILENAME_PLAIN:\"00000001.txt\")",
                     result);
         }
         {
@@ -304,7 +305,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies use correct file name for pdf files
      */
     @Test
-    void generateAccessCheckQuery_shouldUseCorrectFileNameForPdfFiles() {
+    void generateAccessCheckQuery_shouldUseCorrectFileNameForPdfFiles() throws Exception {
         String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "12345.pdf");
         Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME + ":\"12345.pdf\"", result);
     }
@@ -314,7 +315,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies adapt basic alto file name
      */
     @Test
-    void generateAccessCheckQuery_shouldAdaptBasicAltoFileName() {
+    void generateAccessCheckQuery_shouldAdaptBasicAltoFileName() throws Exception {
         String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001.xml");
         Assertions.assertEquals(
                 "+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +(" + SolrConstants.FILENAME_ALTO
@@ -327,13 +328,13 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies escape file name for wildcard search correctly
      */
     @Test
-    void generateAccessCheckQuery_shouldEscapeFileNameForWildcardSearchCorrectly() {
+    void generateAccessCheckQuery_shouldEscapeFileNameForWildcardSearchCorrectly() throws Exception {
         String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001 (1)");
         Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME + ":00000001\\ \\(1\\).*", result);
     }
 
     @Test
-    void generateAccessCheckQuery_shouldUseFullNameForImageFormats() {
+    void generateAccessCheckQuery_shouldUseFullNameForImageFormats() throws Exception {
         {
             String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001.tif");
             Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +(" + SolrConstants.FILENAME + ":\"00000001.tif\" "
@@ -362,7 +363,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
     }
 
     @Test
-    void generateAccessCheckQuery_shouldUseFullNameFor3dObjectFormats() {
+    void generateAccessCheckQuery_shouldUseFullNameFor3dObjectFormats() throws Exception {
         {
             String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001.gltf");
             Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME + ":\"00000001.gltf\"", result);
@@ -378,7 +379,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
     }
 
     @Test
-    void generateAccessCheckQuery_shouldUseBaseNameForFormatlessFiles() {
+    void generateAccessCheckQuery_shouldUseBaseNameForFormatlessFiles() throws Exception {
         {
             String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "00000001");
             Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME + ":00000001.*", result);
@@ -394,7 +395,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies work correctly with urls
      */
     @Test
-    void generateAccessCheckQuery_shouldWorkCorrectlyWithUrls() {
+    void generateAccessCheckQuery_shouldWorkCorrectlyWithUrls() throws Exception {
         String result = AccessConditionUtils.generateAccessCheckQuery("PPN123456789", "file:///opt/digiverso/viewer/cms_media/bild4.png");
         Assertions.assertEquals("+" + SolrConstants.PI_TOPSTRUCT + ":PPN123456789 +" + SolrConstants.FILENAME + ":\"bild4.png\"", result);
     }
@@ -404,7 +405,7 @@ class AccessConditionUtilsTest extends AbstractDatabaseAndSolrEnabledTest {
      * @verifies throw RecordNotFoundException if record not found
      */
     @Test
-    void getPdfDownloadQuotaForRecord_shouldThrowRecordNotFoundExceptionIfRecordNotFound() {
+    void getPdfDownloadQuotaForRecord_shouldThrowRecordNotFoundExceptionIfRecordNotFound() throws Exception {
         Assertions.assertThrows(RecordNotFoundException.class, () -> AccessConditionUtils.getPdfDownloadQuotaForRecord("notfound"));
     }
 
