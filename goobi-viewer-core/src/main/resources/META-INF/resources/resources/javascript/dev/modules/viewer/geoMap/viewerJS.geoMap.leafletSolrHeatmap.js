9326381fa7ba1cec273d11891343da5ddc63e588
JHG <jan.guenther@intranda.com>
2025-12-11 09:52:30 +0100

    remove comment from solrheatmap.js

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/viewer/geoMap/viewerJS.geoMap.leafletSolrHeatmap.js
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/viewer/geoMap/viewerJS.geoMap.leafletSolrHeatmap.js
@@ -201,13 +201,13 @@ if (typeof L !== 'undefined') {
 	  initialize: function(heatmapUrl, featureUrl, featureGroup, options) {
 	    var _this = this;
 	    L.setOptions(_this, options);
-		console.log("init solrHeatmap ", this.options);
+		// console.log("init solrHeatmap ", this.options);
 	    _this.featureGroup = featureGroup;
 	    _this.queryAdapter = new L.SolrHeatmapQueryAdapters[this.options.queryAdapter](this.options, _this);
 	    _this._heatmapUrl = heatmapUrl;
 	    _this.featureUrl = featureUrl;
 	    _this._layers = {};
-	  },
+	  }, 
 	
 	  onAdd: function (map) {
 	    // Call the parent function
