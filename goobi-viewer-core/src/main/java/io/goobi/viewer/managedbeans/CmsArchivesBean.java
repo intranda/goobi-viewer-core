31b21c51e6b1690a825405a54b36ab59349d39f3
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2026-02-03 17:03:40 +0100

    removed duplicate import; refs #27547

--- goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/CmsArchivesBean.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/managedbeans/CmsArchivesBean.java
@@ -25,7 +25,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
