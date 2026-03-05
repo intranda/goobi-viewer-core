82c8fe6c4b67e8db7e4b5193259f9869342bfb2d
JHG <jan.guenther@intranda.com>
2026-03-05 11:39:26 +0100

    remove console log for voyager init

--- goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/media/voyager3DViewer.mjs
+++ goobi-viewer-core/src/main/resources/META-INF/resources/resources/javascript/dev/modules/media/voyager3DViewer.mjs
@@ -15,7 +15,7 @@ export default class Voyager3dView {
     constructor(config) {
         this.config = jQuery.extend(true, {}, _default, config);
         this.container = document.querySelector(this.config.container);
-        console.log('init voyager3d', this);
+       // console.log('init voyager3d', this);
         if (this.isVisible()) {
             this.loaded = this.initView().then(() => {
                 if (_debug) console.log('object data loaded');
