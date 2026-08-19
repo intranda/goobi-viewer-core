c249e6f095fa99a77300c3ba21dcf9a69ab77938
Lars Geiger <lars.geiger@intranda.com>
2026-08-19 17:50:55 +0200

    patch release for 24.09.4

--- goobi-viewer-core/src/main/java/io/goobi/viewer/filters/UrlRedirectFilter.java
+++ goobi-viewer-core/src/main/java/io/goobi/viewer/filters/UrlRedirectFilter.java
@@ -33,6 +33,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
